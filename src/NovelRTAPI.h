8846dfc45f303a1cd6f1f4ee92a0b364514e7f3a
Kirill Mironov <arkrait04@gmail.com>
2019-02-14 22:42:45 +0300

    change to lib and add extern methods

---
+++ src/NovelRTAPI.h
@@ -0,0 +1,12 @@
+//
+// Created by miron on 2/14/19.
+//
+
+#ifndef LIBNOVELRT_NOVELRTAPI_H
+#define LIBNOVELRT_NOVELRTAPI_H
+
+#include "NovelRunner.h"
+
+extern "C" int runNovel(int);
+
+#endif //LIBNOVELRT_NOVELRTAPI_H
