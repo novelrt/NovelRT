6f021892667fd041ed740ac2c5280bbbffc85bad
Kirill Mironov <arkrait04@gmail.com>
2019-02-20 13:31:57 +0300

    begin writing c wrappers

--- src/NovelRunner_C.cpp
+++ src/NovelRunner_C.cpp
@@ -2,4 +2,31 @@
 // Created by miron on 2/20/19.
 //
 
+#include "NovelRunner.h"
 #include "NovelRunner_C.h"
+
+extern "C" {
+    NovelRunner_t *createRunner(int displayNumber) {
+        return reinterpret_cast<NovelRunner_t*>(new NovelRT::NovelRunner(displayNumber));
+    }
+
+    void destroyRunner(NovelRunner_t* runner) {
+        delete reinterpret_cast<NovelRT::NovelRunner*>(runner);
+    }
+
+    void executeUpdateSubscriptions(NovelRunner_t* runner, const float deltaTime) {
+        return reinterpret_cast<NovelRT::NovelRunner*>(runner)->executeUpdateSubscriptions(deltaTime);
+    }
+
+    void runOnUpdate(NovelRunner_t* runner, NovelSubscriber subscriber) {
+        return reinterpret_cast<NovelRT::NovelRunner*>(runner)->runOnUpdate(subscriber);
+    }
+
+    void stopRunningOnUpdate(NovelRunner_t* runner, NovelSubscriber subscriber) {
+        return reinterpret_cast<NovelRT::NovelRunner*>(runner)->stopRunningOnUpdate(subscriber);
+    }
+
+    int runNovel(NovelRunner_t* runner) {
+        return reinterpret_cast<NovelRT::NovelRunner*>(runner)->runNovel();
+    }
+}
