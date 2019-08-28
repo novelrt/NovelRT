2b1d7e31e2ac113e79bd59a4d6024446823cf680
Matt Jones <matthewj866@gmail.com>
2019-08-28 17:56:32 +0100

    piped RGBAConfig into shader for BasicFillRect.

--- src/BasicFragmentShader.glsl
+++ src/BasicFragmentShader.glsl
@@ -1,6 +1,6 @@
 #version 330 core
-in vec3 fragmentColour;
-out vec3 colour;
+in vec4 fragmentColour;
+out vec4 colour;
 void main(){
     colour = fragmentColour;
 }
--- src/BasicVertexShader.glsl
+++ src/BasicVertexShader.glsl
@@ -1,9 +1,9 @@
 #version 330 core
 
 layout(location = 0) in vec3 vertexPosition_modelspace;
-layout(location = 1) in vec3 vertexColour;
+layout(location = 1) in vec4 vertexColour;
 
-out vec3 fragmentColour;
+out vec4 fragmentColour;
 
 void main(){
     gl_Position.xyz = vertexPosition_modelspace;
