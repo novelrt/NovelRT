e9d8e999a3ed7bb4e861e44fdd47b26b11eaef73
Matt Jones <matthewj866@gmail.com>
2019-08-29 02:12:24 +0100

    Part 2 of texture loading. Texture loads but won't render.

--- src/NovelImageRect.cpp
+++ src/NovelImageRect.cpp
@@ -29,12 +29,47 @@ void NovelImageRect::drawObject() const {
 
 
   glUseProgram(_programId);
+  glBindTexture(GL_TEXTURE_2D, _textureId);
+  glBindVertexArray(_vertexArrayObject);
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, _buffer);
+  glVertexAttribPointer(
+      0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+      3,                  // size
+      GL_FLOAT,           // type
+      GL_FALSE,           // normalized?
+      0,                  // stride
+      nullptr          // array buffer offset
+  );
+
+  glEnableVertexAttribArray(1);
+  glBindBuffer(GL_ARRAY_BUFFER, _uvBuffer);
+  glVertexAttribPointer(
+      1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+      2,                                // size
+      GL_FLOAT,                         // type
+      GL_FALSE,                         // normalized?
+      0,                                // stride
+      nullptr                        // array buffer offset
+  );
+// Draw the triangle !
+  glDrawArrays(GL_TRIANGLES, 0, 6); // Starting from vertex 0; 3 vertices total -> 1 triangle
+  //auto bla = glGetError();
+  glDisableVertexAttribArray(1);
+  glDisableVertexAttribArray(0);
+  glBindVertexArray(0);
+
 
 }
+
 void NovelImageRect::configureObjectBuffers(const bool refreshBuffers) {
   NovelRenderObject::configureObjectBuffers(refreshBuffers);
 
   SDL_Surface* surface = IMG_Load("test-yuri.png");
+
+  if(_bufferInitialised) {
+    glDeleteTextures(1, &_textureId);
+  }
 
   glGenTextures(1, &_textureId);
   glBindTexture(GL_TEXTURE_2D, _textureId);
@@ -45,10 +80,13 @@ void NovelImageRect::configureObjectBuffers(const bool refreshBuffers) {
     mode = GL_RGBA;
   }
 
-  glTexImage2D(GL_TEXTURE_2D, 0, mode, surface->w, surface->h, 0, mode, GL_UNSIGNED_BYTE, surface->pixels);
-
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  glTexImage2D(GL_TEXTURE_2D, 0, mode, surface->w, surface->h, 0, mode, GL_UNSIGNED_BYTE, surface->pixels);
+  glGenerateMipmap(GL_TEXTURE_2D);
 
   _uvCoordinates = {
     0.0f, 1.0f,
@@ -58,6 +96,12 @@ void NovelImageRect::configureObjectBuffers(const bool refreshBuffers) {
     0.0f, 0.0f,
     1.0f, 0.0f
   };
+
+  glGenBuffers(1, &_uvBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, _buffer);
+
+  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * _uvCoordinates.size(), _uvCoordinates.data(), GL_STATIC_DRAW);
+  SDL_free(surface);
 
 }
 }
