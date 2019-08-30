24c8b82664df5f5f64905fc0d1838ee774a5fe06
Matt Jones <matthewj866@gmail.com>
2019-08-30 04:51:32 +0100

    Added glm and Freetype as dependencies.

--- src/NovelTextRect.cpp
+++ src/NovelTextRect.cpp
@@ -15,4 +15,20 @@ RGBAConfig NovelTextRect::getColourConfig() const {
 void NovelTextRect::setColourConfig(const RGBAConfig& value) {
   _colourConfig = value;
 }
+void NovelTextRect::configureObjectBuffers(const bool refreshBuffers) {
+  NovelRenderObject::configureObjectBuffers(refreshBuffers);
+}
+NovelTextRect::NovelTextRect(NovelLayeringService* layeringService,
+                             const float fontSize,
+                             const float screenScale,
+                             const std::string& fontFileDir,
+                             RGBAConfig& colourConfig,
+                             const NovelCommonArgs& args,
+                             GLuint programId) : NovelRenderObject(layeringService,
+                                                                   screenScale,
+                                                                   GeoVector<float>(0, 0),
+                                                                   args,
+                                                                   programId), _colourConfig(colourConfig), _fontFileDir(fontFileDir), _fontSize(fontSize) {
+
+}
 }
