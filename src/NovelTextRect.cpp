52e8a7b1829a47a0f99e4f06779f3c588fc9fb82
Matt Jones <matthewj866@gmail.com>
2019-09-04 01:30:08 +0100

    [NOT WORKING] Part 1 of text rendering.

--- src/NovelTextRect.cpp
+++ src/NovelTextRect.cpp
@@ -27,6 +27,47 @@ void NovelTextRect::configureObjectBuffers(const bool refreshBuffers) {
 
     FT_Set_Pixel_Sizes(face, 0, _fontSize);
 
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // Disable byte-alignment restriction
+
+    for (GLubyte c = 0; c < 128; c++)
+    {
+      // Load character glyph
+      if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+      {
+        std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+        continue;
+      }
+      // Generate texture
+      GLuint textureId;
+      glGenTextures(1, &textureId);
+      glBindTexture(GL_TEXTURE_2D, textureId);
+      glTexImage2D(
+          GL_TEXTURE_2D,
+          0,
+          GL_RED,
+          face->glyph->bitmap.width,
+          face->glyph->bitmap.rows,
+          0,
+          GL_RED,
+          GL_UNSIGNED_BYTE,
+          face->glyph->bitmap.buffer
+      );
+      // Set texture options
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      // Now store character for later use
+      GraphicsCharacterRenderData character = {
+
+          textureId,
+          GeoVector<int>(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+          GeoVector<int>(face->glyph->bitmap_left, face->glyph->bitmap_top),
+          face->glyph->advance.x
+      };
+      _fontCharacters.insert(std::pair<GLchar, GraphicsCharacterRenderData>(c, character));
+    }
+
   }
 }
 NovelTextRect::NovelTextRect(NovelLayeringService* layeringService,
