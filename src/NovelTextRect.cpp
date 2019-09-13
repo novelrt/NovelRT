23882950b4d64362a667b296c138ce9fa0ad31e7
Matt Jones <matthewj866@gmail.com>
2019-09-13 18:55:00 +0100

    Textures no longer constantly reload.

--- src/NovelTextRect.cpp
+++ src/NovelTextRect.cpp
@@ -23,58 +23,62 @@ void NovelTextRect::setColourConfig(const RGBAConfig& value) {
 
 void NovelTextRect::configureObjectBuffers() {
 
-  if(_fontCharacters.size() > 0) _fontCharacters.clear();
+  if (_fontCharacters.size() > 0)
+    _fontCharacters.clear();
 
-  FT_Library freeTypeLoader;
-  if (FT_Init_FreeType(&freeTypeLoader)) {
-    std::cerr << "ERROR: Failed to initialise Freetype." << std::endl;
-  }
-  FT_Face face;
-  if (FT_New_Face(freeTypeLoader, _fontFileDir.c_str(), 0, &face))
-    std::cout << "ERROR: Failed to load font " << _fontFileDir << " via freeType!" << std::endl;
-
-  FT_Set_Pixel_Sizes(face, 0, _fontSize);
-
-  glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // Disable byte-alignment restriction
-
-
-  for (GLubyte c = 0; c < 128; c++) {
-    // Load character glyph
-    if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
-      std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
-      continue;
+  if (_previousFontFileDir != _fontFileDir) {
+    FT_Library freeTypeLoader;
+    if (FT_Init_FreeType(&freeTypeLoader)) {
+      std::cerr << "ERROR: Failed to initialise Freetype." << std::endl;
     }
-    // Generate texture
-    GLuint textureId;
-    glGenTextures(1, &textureId);
-    glBindTexture(GL_TEXTURE_2D, textureId);
-    glTexImage2D(
-        GL_TEXTURE_2D,
-        0,
-        GL_RED,
-        face->glyph->bitmap.width,
-        face->glyph->bitmap.rows,
-        0,
-        GL_RED,
-        GL_UNSIGNED_BYTE,
-        face->glyph->bitmap.buffer
-    );
-    // Set texture options
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // Now store character for later use
-    GraphicsCharacterRenderData character = {
-        textureId,
-        GeoVector<int>(face->glyph->bitmap.width, face->glyph->bitmap.rows),
-        GeoVector<int>(face->glyph->bitmap_left, face->glyph->bitmap_top),
-        static_cast<AdvanceInteger>(face->glyph->advance.x)
-    };
-    _fontCharacters.insert(std::pair<GLchar, GraphicsCharacterRenderData>(c, character));
+    FT_Face face;
+    if (FT_New_Face(freeTypeLoader, _fontFileDir.c_str(), 0, &face))
+      std::cout << "ERROR: Failed to load font " << _fontFileDir << " via freeType!" << std::endl;
+
+    FT_Set_Pixel_Sizes(face, 0, _fontSize);
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // Disable byte-alignment restriction
+
+
+    for (GLubyte c = 0; c < 128; c++) {
+      // Load character glyph
+      if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+        std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+        continue;
+      }
+      // Generate texture
+      GLuint textureId;
+      glGenTextures(1, &textureId);
+      glBindTexture(GL_TEXTURE_2D, textureId);
+      glTexImage2D(
+          GL_TEXTURE_2D,
+          0,
+          GL_RED,
+          face->glyph->bitmap.width,
+          face->glyph->bitmap.rows,
+          0,
+          GL_RED,
+          GL_UNSIGNED_BYTE,
+          face->glyph->bitmap.buffer
+      );
+      // Set texture options
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      // Now store character for later use
+      GraphicsCharacterRenderData character = {
+          textureId,
+          GeoVector<int>(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+          GeoVector<int>(face->glyph->bitmap_left, face->glyph->bitmap_top),
+          static_cast<AdvanceInteger>(face->glyph->advance.x)
+      };
+      _fontCharacters.insert(std::pair<GLchar, GraphicsCharacterRenderData>(c, character));
+    }
+    FT_Done_Face(face);
+    FT_Done_FreeType(freeTypeLoader);
+    _previousFontFileDir = _fontFileDir;
   }
-  FT_Done_Face(face);
-  FT_Done_FreeType(freeTypeLoader);
 
   reloadText();
 }
