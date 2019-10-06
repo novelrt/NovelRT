1cf3526c10dad5645c9c9e76fe10e363c333a833
Nejc <ethoarx@gmail.com>
2019-10-06 20:03:49 +0200

    Make ninja ignore the index.html file
    With this change the documentation generation can be ran without deleting docs/html/

--- docs/html/menudata.js
+++ docs/html/menudata.js
@@ -28,6 +28,7 @@ var menudata={children:[
 {text:"Class Hierarchy",url:"hierarchy.html"},
 {text:"Class Members",url:"functions.html",children:[
 {text:"All",url:"functions.html"},
-{text:"Functions",url:"functions_func.html"}]}]},
+{text:"Functions",url:"functions_func.html"},
+{text:"Variables",url:"functions_vars.html"}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"}]}]}
