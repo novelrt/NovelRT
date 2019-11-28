8bcbcb31333b8339f3d3fcbe1b6d23a7968cb127
Matt Jones <matthewj866@gmail.com>
2019-11-28 11:00:33 +0000

    Refactoed BasicVertexShader. Updated TexturedVertexShader to support UBOs.

--- src/BasicVertexShader.glsl
+++ src/BasicVertexShader.glsl
@@ -3,14 +3,13 @@
 layout(location = 0) in vec3 vertexPosition;
 layout(location = 1) in vec4 vertexColour;
 
-layout (std140) uniform FinalViewMatrixBuffer {
+layout (std140) uniform finalViewMatrixBuffer {
   mat4 modelViewProjection
 };
 
 out vec4 fragmentColour;
 
 void main(){
-    vec4 vertexPosition4 = vec4(vertexPosition, 1.0f);
-    gl_Position = vertexPosition4 * modelViewProjection;
+    gl_Position = vec4(vertexPosition, 1.0f) * modelViewProjection;
     fragmentColour = vertexColour;
 }
--- src/TexturedVertexShader.glsl
+++ src/TexturedVertexShader.glsl
@@ -1,14 +1,19 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;
+
+layout (location = 0) in vec3 vertexPosition;
 layout (location = 1) in vec2 aTexCoord;
 layout (location = 2) in vec4 aColourTint;
+
+layout (std140) uniform finalViewMatrixBuffer {
+  mat4 modelViewProjection
+};
 
 out vec2 texCoord;
 out vec4 colourTint;
 
 void main()
 {
-    gl_Position = vec4(aPos, 1.0);
+    gl_Position = vec4(vertexPosition, 1.0) * modelViewProjection;
     texCoord = aTexCoord;
     colourTint = aColourTint;
 }
