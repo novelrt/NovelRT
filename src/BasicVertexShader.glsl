5c8a811a0d0efcf4b5dba40ff8b2b0f0cb61c94b
Matt Jones <matthewj866@gmail.com>
2019-11-28 00:26:26 +0000

    Added UBO changes to BasicVertexShader.

--- src/BasicVertexShader.glsl
+++ src/BasicVertexShader.glsl
@@ -1,14 +1,16 @@
 #version 330 core
 
-layout(location = 0) uniform mat3 projectionView;
-layout(location = 1) uniform mat3 modelTransform;
-layout(location = 2) in vec3 vertexPositionModelSpace;
-layout(location = 3) in vec4 vertexColour;
+layout(location = 0) in vec3 vertexPosition;
+layout(location = 1) in vec4 vertexColour;
+
+layout (std140) uniform FinalViewMatrixBuffer {
+  mat4 modelViewProjection
+};
 
 out vec4 fragmentColour;
 
 void main(){
-    gl_Position.xyz = vertexPositionModelSpace * modelTransform * projectionView;
-    gl_Position.w = 1.0;
+    vec4 vertexPosition4 = vec4(vertexPosition, 1.0f);
+    gl_Position = vertexPosition4 * modelViewProjection;
     fragmentColour = vertexColour;
 }
