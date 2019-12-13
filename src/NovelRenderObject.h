d5a3f4a7bcba6992814826313611187c500eb4ed
Pheubel <luca.guagliardo@live.nl>
2019-12-13 13:49:17 +0100

    reordered NovelRenderObject.h

--- src/NovelRenderObject.h
+++ src/NovelRenderObject.h
@@ -9,29 +9,32 @@
 #include <memory>
 
 namespace NovelRT {
-class NovelRenderObject : public NovelObject {
-public:
-  NovelRenderObject(NovelLayeringService* layeringService, float screenScale, const GeoVector<float>& size,
-                    const NovelCommonArgs& args, GLuint programId);
+  class NovelRenderObject : public NovelObject {
 
-  void executeObjectBehaviour() final;
-  void setWorldSpaceSize(const GeoVector<float>& value) override;
-  void setRotation(float value) override;
-  void setScale(const GeoVector<float>& value) override;
-  void setWorldSpacePosition(const GeoVector<float>& value) override;
-  virtual ~NovelRenderObject();
+  protected:
+    Lazy<GLuint> _buffer;
+    Lazy<GLuint> _vertexArrayObject;
+    GLuint _programId;
+    std::vector<GLfloat> _vertexBufferData;
+    bool _bufferInitialised = false;
 
-protected:
-  virtual void drawObject() = 0;
-  virtual void configureObjectBuffers();
-  static GLuint generateStandardBuffer();
+    virtual void drawObject() = 0;
+    virtual void configureObjectBuffers();
+    static GLuint generateStandardBuffer();
 
-  Lazy<GLuint> _buffer;
-  Lazy<GLuint> _vertexArrayObject;
-  GLuint _programId;
-  std::vector<GLfloat> _vertexBufferData;
-  bool _bufferInitialised = false;
+  public:
+    NovelRenderObject(NovelLayeringService* layeringService,
+                      float screenScale,
+                      const GeoVector<float>& size,
+                      const NovelCommonArgs& args,
+                      GLuint programId);
+    virtual ~NovelRenderObject();
 
-};
+    void executeObjectBehaviour() final;
+    void setWorldSpaceSize(const GeoVector<float>& value) override;
+    void setRotation(float value) override;
+    void setScale(const GeoVector<float>& value) override;
+    void setWorldSpacePosition(const GeoVector<float>& value) override;
+  };
 }
 #endif //NOVELRT_NOVELRENDEROBJECT_H
