fa9fff60e1158e601ec4b1dc29602671e7bb8d24
Pheubel <luca.guagliardo@live.nl>
2019-12-13 13:49:43 +0100

    reordered NovelRunner.h

--- src/NovelRunner.h
+++ src/NovelRunner.h
@@ -10,56 +10,57 @@
 #include "NovelAudioService.h"
 
 namespace NovelRT {
-/**
- * The base class for creating a visual novel.
- */
-class NovelRunner {
-public:
   /**
-   * Executes the provided code upon update.
-   *
-   * @param subscriber The code to execute on update.
+   * The base class for creating a visual novel.
    */
-  void runOnUpdate(NovelUpdateSubscriber);
-  /**
-   * Stops the execution of the instantiated NovelRunner at the specified event.
-   *
-   * @param subscriber The event at which the novel should stop running.
-   */
-  void stopRunningOnUpdate(NovelUpdateSubscriber);
+  class NovelRunner {
 
-  /**
-   * Instantiates the NovelRunner class with its presets.
-   *
-   * @param displayNumber The display on which to start the novel.
-   * @param layeringService The NovelLayeringService that NovelRunner should use.
-   * @param targetFrameRate The framerate that should be targeted and capped.
-   */
-  explicit NovelRunner(int, NovelLayeringService*, uint32_t targetFrameRate = 0);
+  private:
+    StepTimer _stepTimer;
+    std::vector<NovelUpdateSubscriber> _updateSubscribers;
+    NovelLayeringService* _layeringService;
+    std::unique_ptr<NovelDebugService> _novelDebugService;
+    std::unique_ptr<NovelRenderingService> _novelRenderer;
+    std::unique_ptr<NovelInteractionService> _novelInteractionService;
+    std::unique_ptr<NovelAudioService> _novelAudioService;
+    int _exitCode = 1;
 
-  /**
-   * Starts the visual novel.
-   * @returns Exit code.
-   */
-  int runNovel();
-  /// The Rendering Service associated with this Runner.
-  NovelRenderingService* getRenderer() const;
-  /// The Interaction Service associated with this Runner
-  NovelInteractionService* getInteractionService() const;
-  /// The Debug Service associated with this Runner.
-  NovelDebugService* getDebugService() const;
-  NovelAudioService* getAudioService() const;
+  public:
+    /**
+     * Instantiates the NovelRunner class with its presets.
+     *
+     * @param displayNumber The display on which to start the novel.
+     * @param layeringService The NovelLayeringService that NovelRunner should use.
+     * @param targetFrameRate The framerate that should be targeted and capped.
+     */
+    explicit NovelRunner(int, NovelLayeringService*, uint32_t targetFrameRate = 0);
 
-private:
-  StepTimer _stepTimer;
-  std::vector<NovelUpdateSubscriber> _updateSubscribers;
-  NovelLayeringService* _layeringService;
-  std::unique_ptr<NovelDebugService> _novelDebugService;
-  std::unique_ptr<NovelRenderingService> _novelRenderer;
-  std::unique_ptr<NovelInteractionService> _novelInteractionService;
-  std::unique_ptr<NovelAudioService> _novelAudioService;
-  int _exitCode = 1;
-};
+    /**
+     * Executes the provided code upon update.
+     *
+     * @param subscriber The code to execute on update.
+     */
+    void runOnUpdate(NovelUpdateSubscriber);
+    /**
+     * Stops the execution of the instantiated NovelRunner at the specified event.
+     *
+     * @param subscriber The event at which the novel should stop running.
+     */
+    void stopRunningOnUpdate(NovelUpdateSubscriber);
+
+    /**
+     * Starts the visual novel.
+     * @returns Exit code.
+     */
+    int runNovel();
+    /// The Rendering Service associated with this Runner.
+    NovelRenderingService* getRenderer() const;
+    /// The Interaction Service associated with this Runner
+    NovelInteractionService* getInteractionService() const;
+    /// The Debug Service associated with this Runner.
+    NovelDebugService* getDebugService() const;
+    NovelAudioService* getAudioService() const;
+  };
 }
 
 #endif //NOVELRT_NOVELRUNNER_H
