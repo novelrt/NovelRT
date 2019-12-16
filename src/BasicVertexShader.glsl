475c0b0dc76d63999ad1a7a9112a4b49b94467a4
Matt Jones <matthewj866@gmail.com>
2019-12-16 23:58:59 +0000

    Made internal shaders legal again. Oops.

--- src/BasicVertexShader.glsl
+++ src/BasicVertexShader.glsl
@@ -4,7 +4,7 @@ layout(location = 0) in vec3 vertexPosition;
 layout(location = 1) in vec4 vertexColour;
 
 layout (std140) uniform finalViewMatrixBuffer {
-  mat4 modelViewProjection
+  mat4 modelViewProjection;
 };
 
 out vec4 fragmentColour;
