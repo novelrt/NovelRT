fb55bc675b2acb8c7fefc4b27579eb871848bbae
Matt Jones <matthewj866@gmail.com>
2019-12-18 00:25:52 +0000

    fixed indentation in NovelCamera.

--- src/NovelCamera.h
+++ src/NovelCamera.h
@@ -8,55 +8,55 @@
 #include "CameraViewChangedEventArgs.h"
 
 namespace NovelRT {
-class NovelCamera {
+  class NovelCamera {
 
-  NOVELRT_EVENT(CameraViewChanged, CameraViewChangedEventArgs)
+    NOVELRT_EVENT(CameraViewChanged, CameraViewChangedEventArgs)
 
-  //TODO: Look Im just trying to make this legal code okay?
-public:
-  enum class CameraMode : unsigned int {
-    None,
-    Orthographic,
-    Perspective
+      //TODO: Look Im just trying to make this legal code okay?
+  public:
+    enum class CameraMode : unsigned int {
+      None,
+      Orthographic,
+      Perspective
+    };
+
+  private:
+
+    GeoMatrix4<float> _viewMatrix;
+    GeoMatrix4<float> _projectionMatrix;
+    Lazy<GeoMatrix4<float>> _cameraUboMatrix;
+
+    GeoMatrix4<float> generateUboMatrix();
+    CameraMode _cameraMode;
+
+  public:
+
+    NovelCamera();
+
+    inline GeoMatrix4<float> getViewMatrix() const {
+      return _viewMatrix;
+    }
+
+    //TODO: Jeez this is written badly
+    inline void setViewMatrix(GeoMatrix4<float> value) {
+      _cameraUboMatrix.reset();
+      _viewMatrix = value;
+      raiseCameraViewChanged(CameraViewChangedEventArgs(getCameraUboMatrix()));
+    }
+
+    inline GeoMatrix4<float> getProjectionMatrix() const {
+      return _projectionMatrix;
+    }
+
+    inline void setProjectionMatrix(GeoMatrix4<float> value) {
+      _cameraUboMatrix.reset();
+      _projectionMatrix = value;
+      raiseCameraViewChanged(CameraViewChangedEventArgs(getCameraUboMatrix()));
+    }
+
+    inline GeoMatrix4<float> getCameraUboMatrix() {
+      return _cameraUboMatrix.getActual();
+    }
   };
-
-private:
-
-  GeoMatrix4<float> _viewMatrix;
-  GeoMatrix4<float> _projectionMatrix;
-  Lazy<GeoMatrix4<float>> _cameraUboMatrix;
-
-  GeoMatrix4<float> generateUboMatrix();
-  CameraMode _cameraMode;
-
-public:
-
-  NovelCamera();
-
-  inline GeoMatrix4<float> getViewMatrix() const {
-    return _viewMatrix;
-  }
-
-  //TODO: Jeez this is written badly
-  inline void setViewMatrix(GeoMatrix4<float> value) {
-    _cameraUboMatrix.reset();
-    _viewMatrix = value;
-    raiseCameraViewChanged(CameraViewChangedEventArgs(getCameraUboMatrix()));
-  }
-
-  inline GeoMatrix4<float> getProjectionMatrix() const {
-    return _projectionMatrix;
-  }
-
-  inline void setProjectionMatrix(GeoMatrix4<float> value) {
-    _cameraUboMatrix.reset();
-    _projectionMatrix = value;
-    raiseCameraViewChanged(CameraViewChangedEventArgs(getCameraUboMatrix()));
-  }
-
-  inline GeoMatrix4<float> getCameraUboMatrix() {
-    return _cameraUboMatrix.getActual();
-  }
-};
 }
 #endif //NOVELRT_NOVELCAMERA_H
