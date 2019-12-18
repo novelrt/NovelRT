c84c2117818a9e12f94e781497968225241fca10
Matt Jones <matthewj866@gmail.com>
2019-12-18 00:29:11 +0000

    Fixed indentation in NovelWorldObject. Removed dead code from NovelWorldObject.

--- src/NovelWorldObject.cpp
+++ src/NovelWorldObject.cpp
@@ -3,78 +3,67 @@
 #include "GeoBounds.h"
 
 namespace NovelRT {
-NovelWorldObject::NovelWorldObject(NovelLayeringService* layeringService,
-                                   const NovelCommonArgs& args) :
+  NovelWorldObject::NovelWorldObject(NovelLayeringService* layeringService,
+    const NovelCommonArgs& args) :
     _layeringService(layeringService) {
-  setLayer(args.layer);
-  setOrderInLayer(args.orderInLayer);
-  _layeringService->updateLayerInfo(getLayer(), this, true);
-  setPosition(args.startingPosition);
-  setRotation(args.startingRotation);
-  setScale(args.startingScale);
-  setActive(true);
-}
-
-NovelWorldObject::~NovelWorldObject() = default;
-
-GeoVector<float> NovelWorldObject::getPosition() const {
-  return _position;
-}
-
-void NovelWorldObject::setPosition(const GeoVector<float>& value) {
-  _position = value;
-}
-
-float NovelWorldObject::getRotation() const {
-  return _rotation;
-}
-
-void NovelWorldObject::setRotation(const float value) {
-  _rotation = value;
-}
-
-GeoVector<float> NovelWorldObject::getScale() const {
-  return _scale;
-}
-
-void NovelWorldObject::setScale(const GeoVector<float>& value) {
-  _scale = value;
-}
-
-bool NovelWorldObject::getActive() const {
-  return _active;
-}
-
-void NovelWorldObject::setActive(const bool value) {
-  _active = value;
-}
-
-int NovelWorldObject::getLayer() const {
-  return _layer;
-}
-
-void NovelWorldObject::setLayer(int value) {
-  _layer = value;
-  _layeringService->updateLayerInfo(value, this);
-}
-
-int NovelWorldObject::getOrderInLayer() const {
-  return _orderInLayer;
-}
-
-void NovelWorldObject::setOrderInLayer(int value) {
-  _orderInLayer = value;
-  _layeringService->sortLayerOrder(getLayer());
-}
-
-/*GeoBounds NovelWorldObject::getScreenSpaceObjectBounds() {
-  if(_isDirty) {
-  _isDirty = false;
-  GeoVector<float> position = getScreenSpacePosition();
-  GeoVector<float> size = getScreenSpaceSize();
-  _objectBounds = GeoBounds(position, size, getRotation());
+    setLayer(args.layer);
+    setOrderInLayer(args.orderInLayer);
+    _layeringService->updateLayerInfo(getLayer(), this, true);
+    setPosition(args.startingPosition);
+    setRotation(args.startingRotation);
+    setScale(args.startingScale);
+    setActive(true);
   }
-  return _objectBounds;
-}*/
 
+  NovelWorldObject::~NovelWorldObject() = default;
+
+  GeoVector<float> NovelWorldObject::getPosition() const {
+    return _position;
+  }
+
+  void NovelWorldObject::setPosition(const GeoVector<float>& value) {
+    _position = value;
+  }
+
+  float NovelWorldObject::getRotation() const {
+    return _rotation;
+  }
+
+  void NovelWorldObject::setRotation(const float value) {
+    _rotation = value;
+  }
+
+  GeoVector<float> NovelWorldObject::getScale() const {
+    return _scale;
+  }
+
+  void NovelWorldObject::setScale(const GeoVector<float>& value) {
+    _scale = value;
+  }
+
+  bool NovelWorldObject::getActive() const {
+    return _active;
+  }
+
+  void NovelWorldObject::setActive(const bool value) {
+    _active = value;
+  }
+
+  int NovelWorldObject::getLayer() const {
+    return _layer;
+  }
+
+  void NovelWorldObject::setLayer(int value) {
+    _layer = value;
+    _layeringService->updateLayerInfo(value, this);
+  }
+
+  int NovelWorldObject::getOrderInLayer() const {
+    return _orderInLayer;
+  }
+
+  void NovelWorldObject::setOrderInLayer(int value) {
+    _orderInLayer = value;
+    _layeringService->sortLayerOrder(getLayer());
+  }
 }
--- src/NovelWorldObject.h
+++ src/NovelWorldObject.h
@@ -1,4 +1,5 @@
 // Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root for more information.
+
 #ifndef NOVELRT_NOVELWORLDOBJECT_H
 #define NOVELRT_NOVELWORLDOBJECT_H
 #include "GeoVector.h"
