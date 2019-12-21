47603a1146bf9040961969832d019b31094aec28
Matt Jones <matthewj866@gmail.com>
2019-12-21 00:20:43 +0000

    Fixed missing licence comments.

--- src/NovelWindowingService.h
+++ src/NovelWindowingService.h
@@ -1,3 +1,4 @@
+// Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root for more information.
 #ifndef NOVELRT_NOVELWINDOWINGSERVICE_H
 #define NOVELRT_NOVELWINDOWINGSERVICE_H
 #include <SDL2/SDL.h>
