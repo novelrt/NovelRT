7d85a4d7d30f3b5eb921c86eef57a9aded937381
capnkenny <k.rjohnson94@outlook.com>
2020-01-09 06:52:00 -0500

    Updated shaders to match GL ES 3.0 spec

--- src/Resources/Shaders/BasicFragmentShader.glsl
+++ src/Resources/Shaders/BasicFragmentShader.glsl
@@ -1,5 +1,6 @@
 // Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root for more information.
-#version 330 core
+#version 300 es
+precision mediump float;
 in vec4 fragmentColour;
 out vec4 colour;
 void main(){
