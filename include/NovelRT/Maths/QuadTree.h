b0b4dd7976353d91b63522b3111ab5adec2913f6
Tanner Gooding <tagoo@outlook.com>
2020-02-15 10:28:02 -0800

    Implement QuadTree:subdivide

--- include/NovelRT/Maths/QuadTree.h
+++ include/NovelRT/Maths/QuadTree.h
@@ -6,53 +6,69 @@
 #endif
 
 namespace NovelRT::Maths {
-  template <typename T>
+  template <typename TQuadTreePoint>
   class QuadTree {
   private:
-    static const int32_t NODE_CAPACITY = 4;
+    static const int32_t CAPACITY = 4;
 
-    GeoBounds _treeBounds;
-    std::vector<T*> _nodeObjects;
-    std::unique_ptr<QuadTree> _topLeft;
-    std::unique_ptr<QuadTree> _topRight;
-    std::unique_ptr<QuadTree> _bottomLeft;
-    std::unique_ptr<QuadTree> _bottomRight;
+    static const int32_t TOP_LEFT = 0;
+    static const int32_t TOP_RIGHT = 1;
+    static const int32_t BOTTOM_LEFT = 2;
+    static const int32_t BOTTOM_RIGHT = 3;
+
+    GeoBounds _bounds;
+    TQuadTreePoint* _points[CAPACITY];
+    std::unique_ptr<QuadTree> _children[4];
 
   public:
-    explicit QuadTree(GeoBounds treeBounds) noexcept :
-      _treeBounds(treeBounds),
-      _nodeObjects(std::vector<T>()),
-      _topLeft(std::unique_ptr<QuadTree>(nullptr)),
-      _topRight(std::unique_ptr<QuadTree>(nullptr)),
-      _bottomLeft(std::unique_ptr<QuadTree>(nullptr)),
-      _bottomRight(std::unique_ptr<QuadTree>(nullptr)) {
-      static_assert(std::is_base_of<QuadTreeNode, T>::value, "Type argument for T must inherit NovelRT::Maths::QuadTreeNode!");
+    explicit QuadTree(GeoBounds bounds) noexcept :
+      _bounds(bounds),
+      _points({}),
+      _children({}) {
+      static_assert(std::is_base_of<QuadTreePoint, TQuadTreePoint>::value, "Type argument for TQuadTreePoint must inherit NovelRT::Maths::QuadTreePoint!");
     }
 
-    bool tryInsertNodeObject(const T* const target) noexcept {
-      if (target == nullptr || !_treeBounds.pointIsWithinBounds(target->getPosition())) return false;
+    bool tryInsert(const TQuadTreePoint* const point) noexcept {
+      if (point == nullptr || !_treeBounds.pointIsWithinBounds(point->getPosition())) return false;
 
-      if (_nodeObjects.size() < NODE_CAPACITY && _topLeft == nullptr) {
-        _nodeObjects.emplace_back(target);
-        return true;
+      if (_children[TOP_LEFT] == nullptr) {
+        if (_nodeObjects.size() < NODE_CAPACITY) {
+          _nodeObjects.emplace_back(point);
+          return true;
+        }
+        subdivideTree();
       }
 
-      if (_topLeft == nullptr) subdivideTree();
+      auto result = _children[TOP_LEFT]->tryInsert(point) ||
+                    _children[TOP_RIGHT]->tryInsert(point) ||
+                    _children[BOTTOM_LEFT]->tryInsert(point) ||
+                    _children[BOTTOM_RIGHT]->tryInsert(point);
 
-      //TODO: Refactor this omd
-      if (_topLeft->tryInsertNodeObject(target)) return true;
-      if (_topRight->tryInsertNodeObject(target)) return true;
-      if (_bottomLeft->tryInsertNodeObject(target)) return true;
-      if (_bottomRight->tryInsertNodeObject(target)) return true;
-
-      //it should never reach here, but we also can't really afford to throw in this particular code path I don't think
-      return false;
+      assert(result);
+      return result;
     }
 
     void subdivideTree() noexcept {
+      const GeoVector<float> TOP_LEFT_SCALE = GeoVector<float>(-0.5, +0.5);
+      const GeoVector<float> TOP_RIGHT_SCALE = GeoVector<float>(+0.5, +0.5);
+      const GeoVector<float> BOTTOM_LEFT_SCALE = GeoVector<float>(-0.5, -0.5);
+      const GeoVector<float> BOTTOM_RIGHT_SCALE = GeoVector<float>(+0.5, -0.5);
 
+      GeoVector size = _bounds.getSize() / 2;
+      GeoVector position = _bounds.getPosition();
+
+      _children[TOP_LEFT] = std::make_unique(new QuadTree(GeoBounds(position + (size * TOP_LEFT_SCALE), size, 0)));
+      _children[TOP_RIGHT] = std::make_unique(new QuadTree(GeoBounds(position + (size * TOP_RIGHT_SCALE), size, 0)));
+      _children[BOTTOM_LEFT] = std::make_unique(new QuadTree(GeoBounds(position + (size * BOTTOM_LEFT_SCALE), size, 0)));
+      _children[BOTTOM_RIGHT] = std::make_unique(new QuadTree(GeoBounds(position + (size * BOTTOM_RIGHT_SCALE), size, 0)));
+
+      for (auto point : _points) {
+        auto result = tryInsert(point);
+        assert(result);
+      }
+
+      _points = {};
     }
-
   };
 }
 
