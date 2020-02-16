3b8613691dcf9baea64739fad7865e7c773532db
Tanner Gooding <tagoo@outlook.com>
2020-02-15 16:30:51 -0800

    Adding a getIntersectingPoints method to the quad tree

--- include/NovelRT/Maths/QuadTree.h
+++ include/NovelRT/Maths/QuadTree.h
@@ -16,7 +16,7 @@ namespace NovelRT::Maths {
     static const int32_t BOTTOM_LEFT = 2;
     static const int32_t BOTTOM_RIGHT = 3;
 
-    typedef std::array<TQuadTreePoint*, POINT_CAPACITY> TPoints;
+    typedef std::array<std::shared_ptr<TQuadTreePoint>, POINT_CAPACITY> TPoints;
     typedef std::array<std::unique_ptr<QuadTree>, 4> TChildren;
 
     GeoBounds _bounds;
@@ -25,10 +25,10 @@ namespace NovelRT::Maths {
     uint32_t _pointCount;
 
   public:
-    explicit QuadTree(GeoBounds bounds) noexcept :
+    explicit QuadTree(const GeoBounds& bounds) noexcept :
       _bounds(bounds),
-      _points(TPoints()),
-      _children(TChildren()),
+      _points({}),
+      _children({}),
       _pointCount(0) {
       static_assert(std::is_base_of<QuadTreePoint, TQuadTreePoint>::value, "Type argument for TQuadTreePoint must inherit NovelRT::Maths::QuadTreePoint!");
     }
@@ -37,7 +37,7 @@ namespace NovelRT::Maths {
       return _bounds;
     }
 
-    const TQuadTreePoint* getPoint(uint32_t index) const noexcept {
+    std::shared_ptr<TQuadTreePoint> getPoint(uint32_t index) const noexcept {
       return (index < _pointCount) ? _points[index] : nullptr;
     }
 
@@ -61,7 +61,7 @@ namespace NovelRT::Maths {
       return _children[BOTTOM_RIGHT].get();
     }
 
-    bool tryInsert(TQuadTreePoint* point) noexcept {
+    bool tryInsert(std::shared_ptr<TQuadTreePoint> point) noexcept {
       if (point == nullptr || !getBounds().pointIsWithinBounds(point->getPosition())) return false;
 
       if (_children[TOP_LEFT] == nullptr) {
@@ -90,17 +90,44 @@ namespace NovelRT::Maths {
       GeoVector size = getBounds().getSize() / 2;
       GeoVector position = getBounds().getPosition();
 
-      _children[TOP_LEFT] = std::unique_ptr<QuadTree>(new QuadTree(GeoBounds(position + (size * TOP_LEFT_SCALE), size, 0)));
-      _children[TOP_RIGHT] = std::unique_ptr<QuadTree>(new QuadTree(GeoBounds(position + (size * TOP_RIGHT_SCALE), size, 0)));
-      _children[BOTTOM_LEFT] = std::unique_ptr<QuadTree>(new QuadTree(GeoBounds(position + (size * BOTTOM_LEFT_SCALE), size, 0)));
-      _children[BOTTOM_RIGHT] = std::unique_ptr<QuadTree>(new QuadTree(GeoBounds(position + (size * BOTTOM_RIGHT_SCALE), size, 0)));
+      _children[TOP_LEFT] = std::make_unique<QuadTree>(GeoBounds(position + (size * TOP_LEFT_SCALE), size, 0));
+      _children[TOP_RIGHT] = std::make_unique<QuadTree>(GeoBounds(position + (size * TOP_RIGHT_SCALE), size, 0));
+      _children[BOTTOM_LEFT] = std::make_unique<QuadTree>(GeoBounds(position + (size * BOTTOM_LEFT_SCALE), size, 0));
+      _children[BOTTOM_RIGHT] = std::make_unique<QuadTree>(GeoBounds(position + (size * BOTTOM_RIGHT_SCALE), size, 0));
 
-      for (auto point : _points) {
+      for (uint32_t index = 0; index < getPointCount(); index++) {
+        auto point = getPoint(index);
         auto result = tryInsert(point);
         assert(result);
       }
 
       _pointCount = 0;
+    }
+
+    void getIntersectingPoints(const GeoBounds& bounds, std::vector<std::shared_ptr<TQuadTreePoint>>& intersectingPoints) {
+      if (getBounds().intersectsWith(bounds)) {
+        return;
+      }
+
+      if (_children[TOP_LEFT] == nullptr) {
+        for (uint32_t index = 0; index < getPointCount(); index++) {
+          auto point = getPoint(index);
+          if (bounds.pointIsWithinBounds(point->getPosition())) {
+            intersectingPoints.push_back(point);
+          }
+        }
+      } else {
+        _children[TOP_LEFT]->getIntersectingPoints(bounds, intersectingPoints);
+        _children[TOP_RIGHT]->getIntersectingPoints(bounds, intersectingPoints);
+        _children[BOTTOM_LEFT]->getIntersectingPoints(bounds, intersectingPoints);
+        _children[BOTTOM_RIGHT]->getIntersectingPoints(bounds, intersectingPoints);
+      }
+    }
+
+    std::vector<std::shared_ptr<TQuadTreePoint>> getIntersectingPoints(const GeoBounds& bounds) {
+      auto intersectingPoints = std::vector<std::shared_ptr<TQuadTreePoint>>();
+      getIntersectingPoints(bounds, intersectingPoints);
+      return intersectingPoints;
     }
   };
 }
