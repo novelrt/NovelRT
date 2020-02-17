2f27cf20d95814105d2a9e71ab6609fae94fb6b9
Matt Jones <matthewj866@gmail.com>
2020-02-17 19:34:54 +0000

    [NOT COMPILING] pushing to get clang output on a smart ptr compile error.

--- src/NovelRT/Graphics/TextRect.cpp
+++ src/NovelRT/Graphics/TextRect.cpp
@@ -16,65 +16,6 @@ namespace NovelRT::Graphics {
   }
 
   void TextRect::configureObjectBuffers() {
-
-    if (_previousFontFileDir != _fontFileDir) {
-      if (_fontCharacters.size() > 0)
-        _fontCharacters.clear();
-      FT_Library freeTypeLoader;
-      if (FT_Init_FreeType(&freeTypeLoader)) {
-        _logger.logErrorLine("Failed to initialise Freetype.");
-      }
-      FT_Face face;
-      if (FT_New_Face(freeTypeLoader, _fontFileDir.c_str(), 0, &face))
-        _logger.logError("FREETYPE - Failed to load font: ", _fontFileDir);
-
-      FT_Set_Pixel_Sizes(face, 0, _fontSize);
-
-      glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // Disable byte-alignment restriction
-
-
-      for (GLubyte c = 0; c < 128; c++) {
-        // Load character glyph
-        if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
-          _logger.logErrorLine("FREETYTPE: Failed to load Glyph");
-          continue;
-        }
-        // Generate texture
-        GLuint textureId;
-        glGenTextures(1, &textureId);
-        glBindTexture(GL_TEXTURE_2D, textureId);
-        glTexImage2D(
-          GL_TEXTURE_2D,
-          0,
-          GL_RED,
-          face->glyph->bitmap.width,
-          face->glyph->bitmap.rows,
-          0,
-          GL_RED,
-          GL_UNSIGNED_BYTE,
-          face->glyph->bitmap.buffer
-        );
-        // Set texture options
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        // Now store character for later use
-        GraphicsCharacterRenderData character = {
-            std::make_shared<Texture>(Texture()),
-            Maths::GeoVector<int>(face->glyph->bitmap.width, face->glyph->bitmap.rows),
-            Maths::GeoVector<int>(face->glyph->bitmap_left, face->glyph->bitmap_top),
-            GraphicsCharacterRenderDataHelper::getAdvanceDistance(face->glyph->advance.x)
-        };
-        character.texture->setTextureIdInternal(textureId);
-        _fontCharacters.insert(std::pair<GLchar, GraphicsCharacterRenderData>(c, character));
-      }
-      FT_Done_Face(face);
-      FT_Done_FreeType(freeTypeLoader);
-      _previousFontFileDir = _fontFileDir;
-    }
-
     reloadText();
   }
 
@@ -82,20 +23,17 @@ namespace NovelRT::Graphics {
     int layer,
     ShaderProgram shaderProgram,
     Camera* camera,
-    float fontSize,
-    const std::string& fontFileDir,
+    std::shared_ptr<FontSet> fontSet,
     const RGBAConfig& colourConfig) :
     RenderObject(
       transform,
       layer,
       shaderProgram,
       camera),
-    _fontFileDir(fontFileDir),
-    _previousFontFileDir(""),
     _text(""),
     _logger(Utilities::Misc::CONSOLE_LOG_GFX),
     _colourConfig(colourConfig),
-    _fontSize(fontSize) {}
+    _fontSet(fontSet) {}
 
   std::string TextRect::getText() const {
     return _text;
@@ -128,18 +66,14 @@ namespace NovelRT::Graphics {
     for (const char& c : getText()) {
       GraphicsCharacterRenderData ch;
 
-      auto match = _fontCharacters.find(c);
-      if (match == _fontCharacters.end()) {
-        match = _fontCharacters.begin();
-      }
-      ch = match->second;
+      auto match = _fontSet->getCharacterBasedonGLchar(c);
 
       auto currentWorldPosition = Maths::GeoVector<float>((ttfOrigin.getX() + ch.size.getX() / 2.0f) + ch.bearing.getX(),
         (ttfOrigin.getY() - (ch.bearing.getY() / 2.0f))
         + ((ch.size.getY() - ch.bearing.getY()) / 2.0f));
 
       auto& target = _letterRects.at(i++);
-      target->setTextureInternal(ch.texture);
+      target->setTexture(ch.texture);
       target->getTransform().setPosition(currentWorldPosition);
       target->getTransform().setScale(Maths::GeoVector<float>(ch.size.getX(), ch.size.getY()));
       target->setActive(true);
