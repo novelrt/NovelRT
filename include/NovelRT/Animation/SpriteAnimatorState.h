0424ab8c05703a91de1609c7537eed61f5a93d07
Matt Jones <matthewj866@gmail.com>
2020-02-18 04:46:12 +0000

    Committing before break time. Added most of the SpriteAnimator logic. A LOT of refactoring to do, probably.

--- include/NovelRT/Animation/SpriteAnimatorState.h
+++ include/NovelRT/Animation/SpriteAnimatorState.h
@@ -11,15 +11,37 @@ namespace NovelRT::Animation {
   class SpriteAnimatorState {
   private:
     std::vector<SpriteAnimatorFrame> _frames;
-    std::map<std::string, std::tuple<std::shared_ptr<SpriteAnimatorState>, std::vector<std::function<bool()>>>> _transitions;
+    bool _shouldLoop;
 
   public:
+    std::map<std::string, std::tuple<std::shared_ptr<SpriteAnimatorState>, std::vector<std::function<bool()>>>> transitions; //fuck I give up for now
+
     inline void insertNewState(const std::string& transitionName, std::shared_ptr<SpriteAnimatorState> stateTarget, std::vector<std::function<bool()>> transitionConditions) {
-      _transitions.try_emplace(transitionName, make_tuple(stateTarget, transitionConditions));
+      transitions.try_emplace(transitionName, make_tuple(stateTarget, transitionConditions));
     }
 
     inline void removeState(const std::string& name) { //not sure if we can make this noexcept somehow but whatever
-      _transitions.erase(name);
+      transitions.erase(name);
+    }
+
+    inline bool getShouldLoop() const noexcept {
+      return _shouldLoop;
+    }
+
+    inline void setShouldLoop(bool value) noexcept {
+      _shouldLoop = value;
+    }
+
+    inline const std::vector<SpriteAnimatorFrame>* getFrames() const noexcept {
+      return &_frames;
+    }
+
+    inline void setFrames(const std::vector<SpriteAnimatorFrame>& value) noexcept {
+      _frames = value;
+    }
+
+    inline void setTransitions(const std::map<std::string, std::tuple<std::shared_ptr<SpriteAnimatorState>, std::vector<std::function<bool()>>>>& value) noexcept {
+      transitions = value;
     }
   };
 }
--- src/NovelRT/Animation/SpriteAnimator.cpp
+++ src/NovelRT/Animation/SpriteAnimator.cpp
@@ -4,14 +4,53 @@
 
 namespace NovelRT::Animation {
   SpriteAnimator::SpriteAnimator(NovelRunner* runner, Graphics::ImageRect* rect) noexcept :
-  _runner(runner),
-  _rect(rect),
-  _animatorState(AnimatorPlayState::Stopped),
-  _animationUpdateHandle([=](double delta) { constructAnimation(delta); }) {}
+    _accumulatedDelta(0.0f),
+    _runner(runner),
+    _rect(rect),
+    _animatorState(AnimatorPlayState::Stopped),
+    _animationUpdateHandle([=](double delta) { constructAnimation(delta); }),
+    _currentState(nullptr) {}
 
   void SpriteAnimator::constructAnimation(double delta) {
     switch (_animatorState) {
     case AnimatorPlayState::Playing:
+      if (_currentState == nullptr) {
+        _currentState = _states.at(0);
+        _currentState->getFrames()->at(_currentFrameIndex).getOnFrameEnterDelegate()();
+      }
+
+      if (_currentState->getShouldLoop() && _currentFrameIndex >= _currentState->getFrames()->size()) _currentFrameIndex = 0;
+
+      if (_currentState->getFrames()->size() < _currentFrameIndex && _currentState->getFrames()->at(_currentFrameIndex).getDuration() <= _accumulatedDelta) {
+        _currentState->getFrames()->at(_currentFrameIndex++).getOnFrameExitDelegate()();
+        auto newFrame = _currentState->getFrames()->at(_currentFrameIndex);
+        newFrame.getOnFrameEnterDelegate()();
+        _rect->setTexture(newFrame.getTexture());
+      }
+
+      for (auto pair : _currentState->transitions) {
+        auto conditions = std::get<std::vector<std::function<bool()>>>(pair.second);
+        bool shouldTransitionToThisState = true;
+
+        for (auto condition : conditions) {
+          if (condition()) continue;
+
+          shouldTransitionToThisState = false;
+          break;
+        }
+
+        if (!shouldTransitionToThisState) continue;
+
+        _currentState = std::get<std::shared_ptr<SpriteAnimatorState>>(pair.second);
+        _accumulatedDelta = 0.0f;
+        _currentFrameIndex = 0;
+        _currentState->getFrames()->at(_currentFrameIndex).getOnFrameEnterDelegate()();
+
+        break;
+
+      }
+
+      _accumulatedDelta += delta;
       break;
     case AnimatorPlayState::Paused:
       break;
