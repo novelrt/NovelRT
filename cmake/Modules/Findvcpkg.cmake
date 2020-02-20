3c0de3d1ef4facc529753e705de2d434e59b8d66
FiniteReality <digpoealternate@gmail.com>
2020-02-20 20:36:42 +0000

    Move vcpkg triplet into another variable

--- cmake/Modules/Findvcpkg.cmake
+++ cmake/Modules/Findvcpkg.cmake
@@ -1,7 +1,10 @@
+set(_vcpkg_install_parameters "")
+
 if(WIN32)
   set(_vcpkg_toolchain "${CMAKE_BINARY_DIR}\\vcpkg\\src\\vcpkg\\scripts\\buildsystems\\vcpkg.cmake")
   if(${CMAKE_SIZEOF_VOID_P} EQUAL "8")
-    set(_vcpkg_install_command "${CMAKE_BINARY_DIR}\\vcpkg\\src\\vcpkg\\vcpkg.exe" --triplet x64-windows)
+    set(_vcpkg_install_command "${CMAKE_BINARY_DIR}\\vcpkg\\src\\vcpkg\\vcpkg.exe")
+    set(_vcpkg_install_parameters --triplet x64-windows)
   else()
     set(_vcpkg_install_command "${CMAKE_BINARY_DIR}\\vcpkg\\src\\vcpkg\\vcpkg.exe")
   endif()
@@ -10,7 +13,7 @@ else()
   set(_vcpkg_install_command "${CMAKE_BINARY_DIR}/vcpkg/src/vcpkg/vcpkg")
 endif()
 
-mark_as_advanced(_vcpkg_install_command _vcpkg_toolchain)
+mark_as_advanced(_vcpkg_install_command _vcpkg_toolchain _vcpkg_install_parameters)
 
 if(NOT EXISTS ${_vcpkg_install_command})
   make_directory(${CMAKE_BINARY_DIR}/vcpkg)
@@ -25,7 +28,7 @@ if(NOT EXISTS ${_vcpkg_install_command})
 endif()
 
 execute_process(
-  COMMAND ${_vcpkg_install_command} install ${vcpkg_FIND_COMPONENTS})
+  COMMAND ${_vcpkg_install_command} install ${vcpkg_FIND_COMPONENTS} ${_vcpkg_install_parameters})
 
 include(${_vcpkg_toolchain})
 
