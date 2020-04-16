d7af0f9fdbecd0f8dbb41a3e4a2b63aca947d0a4
Tyler Davis <tgfdavis98@gmail.com>
2020-04-16 19:00:09 +0100

    update main.cpp to use std::filesystem::path.

--- lua-samples/main.cpp
+++ lua-samples/main.cpp
@@ -4,7 +4,16 @@
 
 int main(int /*argc*/, char* /*argv*/[])
 {
-  auto luaRunner = NovelRT::Lua::LuaRunner("./main.lua");
+  std::filesystem::path executableDirPath = NovelRT::Utilities::Misc::getExecutableDirPath();
+  std::filesystem::path resourcesDirPath = executableDirPath / "Resources";
+
+  std::filesystem::path fontsDirPath = resourcesDirPath / "Fonts";
+  std::filesystem::path imagesDirPath = resourcesDirPath / "Images";
+  std::filesystem::path scriptsDirPath = resourcesDirPath / "Scripts";
+  std::filesystem::path soundsDirPath = resourcesDirPath / "Sounds";
+
+
+  auto luaRunner = NovelRT::Lua::LuaRunner((scriptsDirPath / "main.lua").string());
   luaRunner.init();
   luaRunner.run();
   return 0;
