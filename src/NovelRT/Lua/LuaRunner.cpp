e0792911ac196c61bd5f1f873ce3a2f4940b435a
Tyler Davis <tgfdavis98@gmail.com>
2020-04-16 14:10:45 +0100

    add novelrunner type binding.

--- src/NovelRT/Lua/LuaRunner.cpp
+++ src/NovelRT/Lua/LuaRunner.cpp
@@ -13,12 +13,28 @@ namespace NovelRT::Lua {
       lib::debug, lib::io, lib::math, lib::os,
       lib::package, lib::string, lib::table, lib::utf8);
 
-    _state.new_usertype<NovelRT::NovelRunner>("novel_runner"); //, sol::constructors<NovelRT::NovelRunner(int, std::string)>()
-    _state.new_usertype<NovelRT::Transform>("transform"); //, sol::constructors<NovelRT::Transform(NovelRT::Maths::GeoVector2<float>, int, NovelRT::Maths::GeoVector2<float>)>()
-    _state.new_usertype<NovelRT::Maths::GeoVector2<float>>("geo_vector"); //, sol::constructors<NovelRT::Maths::GeoVector2<float>(int, int)>()
+    // NovelRunner
+    auto novelRunnerType = _state.new_usertype<NovelRT::NovelRunner>("NovelRunner",
+      sol::constructors<NovelRT::NovelRunner(int), NovelRT::NovelRunner(int, std::string), NovelRT::NovelRunner(int, int, std::string)>());
+
+    novelRunnerType["run_novel"] = &NovelRunner::runNovel;
+    novelRunnerType["get_renderer"] = &NovelRunner::getRenderer;
+    novelRunnerType["get_interaction_service"] = &NovelRunner::getInteractionService;
+    novelRunnerType["get_audio_service"] = &NovelRunner::getAudioService;
+    novelRunnerType["get_debug_service"] = &NovelRunner::getDebugService;
+    novelRunnerType["get_dot_net_runtime_service"] = &NovelRunner::getDotNetRuntimeService;
+    novelRunnerType["get_windowing_service"] = &NovelRunner::getWindowingService;
+
+    // Transform
+    auto transformType = _state.new_usertype<NovelRT::Transform>("Transform",
+      sol::constructors<NovelRT::Transform(),
+        NovelRT::Transform(const Maths::GeoVector2<float> & position,
+          float rotation, const Maths::GeoVector2<float> & scale)>());
+
+
   }
 
   void LuaRunner::run() {
-    _state.script(_fileName);
+    auto result = _state.script_file(_fileName);
   }
 }
