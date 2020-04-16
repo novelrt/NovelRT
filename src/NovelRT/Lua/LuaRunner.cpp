36f4a98f5ab74d9a8badd40afc1086023bdb5f6f
Tyler Davis <tgfdavis98@gmail.com>
2020-04-16 15:31:22 +0100

    add transform type binding.

--- src/NovelRT/Lua/LuaRunner.cpp
+++ src/NovelRT/Lua/LuaRunner.cpp
@@ -15,7 +15,8 @@ namespace NovelRT::Lua {
 
     // NovelRunner
     auto novelRunnerType = _state.new_usertype<NovelRunner>("NovelRunner",
-      sol::constructors<NovelRunner(int), NovelRunner(int, const std::string&), NovelRunner(int, const std::string&, uint32_t)>());
+      sol::constructors<NovelRunner(int), NovelRunner(int, const std::string&), NovelRunner(int, const std::string&, uint32_t)>()
+      );
 
     novelRunnerType["runNovel"] = &NovelRunner::runNovel;
     novelRunnerType["getRenderer"] = sol::property(&NovelRunner::getRenderer);
@@ -24,6 +25,30 @@ namespace NovelRT::Lua {
     novelRunnerType["getDebugService"] = sol::property(&NovelRunner::getDebugService);
     novelRunnerType["getDotNetRuntimeService"] = sol::property(&NovelRunner::getDotNetRuntimeService);
     novelRunnerType["getWindowingService"] = sol::property(&NovelRunner::getWindowingService);
+
+    // Transform
+    auto transformType = _state.new_usertype<Transform>("Transform",
+      sol::constructors<Transform(),
+      Transform(const Maths::GeoVector2<float> & position,
+        float rotation, const Maths::GeoVector2<float> & scale)>()
+      );
+
+    transformType["getAABB"] = sol::property(&Transform::getAABB);
+    transformType["getBounds"] = sol::property(&Transform::getBounds);
+    transformType["position"] = sol::property(
+      static_cast<const Maths::GeoVector2<float> & (Transform::*)() const>(&Transform::position),
+      static_cast<Maths::GeoVector2<float>& (Transform::*)()>(&Transform::position)
+      );
+
+    transformType["rotation"] = sol::property(
+      static_cast<const float& (Transform::*)() const>(&Transform::rotation),
+      static_cast<float& (Transform::*)()>(&Transform::rotation)
+      );
+
+    transformType["scale"] = sol::property(
+      static_cast<const Maths::GeoVector2<float> & (Transform::*)() const>(&Transform::scale),
+      static_cast<Maths::GeoVector2<float>& (Transform::*)()>(&Transform::scale)
+      );
   }
 
   void LuaRunner::run() {
