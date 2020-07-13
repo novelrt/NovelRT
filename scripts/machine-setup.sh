31dd871fe9770d5c231490126bbd86f7a451549d
FiniteReality <digpoealternate@gmail.com>
2020-07-13 21:59:58 +0100

    I forgot to install libsndfile1-dev on Linux...

--- scripts/machine-setup.sh
+++ scripts/machine-setup.sh
@@ -9,7 +9,7 @@ sudo apt-add-repository 'deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8
 
 sudo apt-get update
 sudo apt-get install clang-8 cmake g++-8 libglu1-mesa-dev libxcursor-dev libxinerama-dev xorg-dev yasm python3 -y
-sudo apt-get install libfreetype6-dev libglfw3-dev libglm-dev libgtest-dev liblua5.3-dev libopenal-dev libspdlog-dev -y
+sudo apt-get install libfreetype6-dev libglfw3-dev libglm-dev libgtest-dev libsndfile1-dev liblua5.3-dev libopenal-dev libspdlog-dev -y
 
 python3 -m pip --version
 python3 -m pip install setuptools wheel
