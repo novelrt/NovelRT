0523859beb899da087385c56dfef201e4d9e9228
Kao <floconetflanelle@gmail.com>
2020-08-06 17:22:50 +0200

    Use of a callback for `forceResize()` Add a check for the window size to avoid dividing by 0

--- include/NovelRT/Graphics/Camera.h
+++ include/NovelRT/Graphics/Camera.h
@@ -17,6 +17,7 @@ namespace NovelRT::Graphics {
     Utilities::Lazy<Maths::GeoMatrix4x4<float>> _cameraUboMatrix;
     Maths::GeoMatrix4x4<float> generateUboMatrix();
     CameraFrameState _cameraFrameState;
+    std::function<void(Camera*, const Maths::GeoVector2<float>&)> _forceResizeCallback;
 
     void initialiseCameraForFrame();
     void forceResize(const Maths::GeoVector2<float>& windowSize);
@@ -50,6 +51,14 @@ namespace NovelRT::Graphics {
 
     inline CameraFrameState getFrameState() const {
       return _cameraFrameState;
+    }
+
+    inline const std::function<void(Camera*, const Maths::GeoVector2<float>&)>& forceResizeCallback() const {
+      return _forceResizeCallback;
+    }
+
+    inline std::function<void(Camera*, const Maths::GeoVector2<float>&)>& forceResizeCallback() {
+      return _forceResizeCallback;
     }
 
     /**
--- src/NovelRT/Graphics/Camera.cpp
+++ src/NovelRT/Graphics/Camera.cpp
@@ -8,17 +8,34 @@ namespace NovelRT::Graphics {
     _cameraFrameState(CameraFrameState::ModifiedInCurrent) {
   }
 
-  std::unique_ptr<Camera> Camera::createDefaultOrthographicProjection(const Maths::GeoVector2<float>& windowSize) {
+  std::unique_ptr<Camera> Camera::createDefaultOrthographicProjection(const Maths::GeoVector2<float>& windowSize) {  
+    auto callback = [](Camera* camera, const Maths::GeoVector2<float>& windowSize) {
+      camera->setProjectionMatrix(Maths::GeoMatrix4x4<float>(glm::ortho<float>(0, windowSize.getX(), windowSize.getY(), 0, 0, 65535)));
+      camera->setViewMatrix(Maths::GeoMatrix4x4<float>(glm::scale(glm::vec3(windowSize.getX() / 1920.0f, windowSize.getY() / 1080.0f, -1.0f))));
+    };
+
     auto returnVal = std::make_unique<Camera>();
-    returnVal->setProjectionMatrix(Maths::GeoMatrix4x4<float>(glm::ortho<float>(0, windowSize.getX(), windowSize.getY(), 0, 0, 65535)));
-    returnVal->setViewMatrix(Maths::GeoMatrix4x4<float>(glm::scale(glm::vec3(windowSize.getX() / 1920.0f, windowSize.getY() / 1080.0f, -1.0f))));
+    returnVal->forceResizeCallback() = callback;
+
+    callback(returnVal.get(), windowSize);
+
     return returnVal;
   }
 
   std::unique_ptr<Camera> Camera::createDefaultPerspectiveProjection(const Maths::GeoVector2<float>& windowSize) {
+    auto callback = [](Camera* camera, const Maths::GeoVector2<float>& windowSize) {
+
+      auto aspect = windowSize.getY() != 0 ? windowSize.getX() / windowSize.getY() : 0;
+      
+      camera->setProjectionMatrix(Maths::GeoMatrix4x4<float>(glm::perspective<float>(glm::radians(90.0f), aspect, 0.1f, 100.0f)));
+      camera->setViewMatrix(Maths::GeoMatrix4x4<float>(glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f))));
+    };
+    
     auto returnVal = std::make_unique<Camera>();
-    returnVal->setProjectionMatrix(Maths::GeoMatrix4x4<float>(glm::perspective<float>(glm::radians(90.0f), windowSize.getX() / windowSize.getY(), 0.1f, 100.0f)));
-    returnVal->setViewMatrix(Maths::GeoMatrix4x4<float>(glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f))));
+    returnVal->forceResizeCallback() = callback;
+
+    callback(returnVal.get(), windowSize);
+
     return returnVal;
   }
 
@@ -40,7 +57,6 @@ namespace NovelRT::Graphics {
   }
 
   void Camera::forceResize(const Maths::GeoVector2<float>& windowSize) {
-    setProjectionMatrix(Maths::GeoMatrix4x4<float>(glm::ortho<float>(0, windowSize.getX(), windowSize.getY(), 0, 0, 65535)));
-    setViewMatrix(Maths::GeoMatrix4x4<float>(glm::scale(glm::vec3(windowSize.getX() / 1920.0f, windowSize.getY() / 1080.0f, -1.0f))));
+    forceResizeCallback()(this, windowSize);
   }
 }
