f1c15c88400912a9f53bde169a654f7d8d109f95
Ken Johnson <k.rjohnson94@outlook.com>
2020-08-13 22:31:42 -0400

    Refactored GeoBounds to POD

---
+++ include/NovelRT.Interop/Maths/NovelRTGeoBounds.h
@@ -0,0 +1,28 @@
+// Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root for more information.
+#include "NovelRT.Interop/Maths/NovelRTGeoVector2F.h"
+#include "NovelRT.Interop/NovelRTInteropUtils.h"
+
+#ifndef NOVELRT_INTEROP_MATHS_GEOBOUNDS_H
+#define NOVELRT_INTEROP_MATHS_GEOBOUNDS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  typedef struct {
+    NovelRTGeoVector2F _position;
+    float _rotation;
+    NovelRTGeoVector2F _size;
+  } NovelRTGeoBounds;
+
+  NovelRTGeoVector2F NovelRT_GeoBounds_getCornerInLocalSpace(const NovelRTGeoBounds bounds, int index);
+  NovelRTGeoVector2F NovelRT_GeoBounds_getCornerInWorldSpace(const NovelRTGeoBounds bounds, int index);
+  NovelRTBool NovelRT_GeoBounds_pointIsWithinBounds(const NovelRTGeoBounds bounds, const NovelRTGeoVector2F point);
+  NovelRTGeoVector2F NovelRT_GeoBounds_getExtents(const NovelRTGeoBounds bounds);
+  NovelRTBool NovelRT_GeoBounds_intersectsWith(const NovelRTGeoBounds first, const NovelRTGeoBounds other);
+
+  
+#ifdef __cplusplus
+}
+#endif
+
+#endif //NOVELRT_INTEROP_MATHS_GEOBOUNDS_H
--- src/NovelRT.Interop/Maths/NovelRTGeoBounds.cpp
+++ src/NovelRT.Interop/Maths/NovelRTGeoBounds.cpp
@@ -1,8 +1,8 @@
 // Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root for more information.
 
 #include "NovelRT.h"
-#include "NovelRT.Interop/Maths/GeoVector2F_t.h"
-#include "NovelRT.Interop/Maths/GeoBounds_t.h"
+#include "NovelRT.Interop/Maths/NovelRTGeoVector2F.h"
+#include "NovelRT.Interop/Maths/NovelRTGeoBounds.h"
 
 
 #ifdef __cplusplus
@@ -10,38 +10,40 @@ extern "C" {
   using namespace NovelRT;
 #endif
 
-  GeoBounds_t GeoBounds_create(const NovelRTGeoVector2F& position, const NovelRTGeoVector2F& size, float rotation) {
-    return GeoBounds_t{ position, rotation, size };
-  }
-
-  NovelRTGeoVector2F GeoBounds_getCornerInLocalSpace(const GeoBounds_t& bounds, int index) {
+  NovelRTGeoVector2F NovelRT_GeoBounds_getCornerInLocalSpace(const NovelRTGeoBounds bounds, int index) {
     const Maths::GeoBounds& cBounds = reinterpret_cast<const Maths::GeoBounds&>(bounds);
     Maths::GeoVector2<float> corner = cBounds.getCornerInLocalSpace(index);
     return reinterpret_cast<NovelRTGeoVector2F&>(corner);
   }
 
-  NovelRTGeoVector2F GeoBounds_getCornerInWorldSpace(const GeoBounds_t& bounds, int index) {
+  NovelRTGeoVector2F NovelRT_GeoBounds_getCornerInWorldSpace(const NovelRTGeoBounds bounds, int index) {
     const Maths::GeoBounds& cBounds = reinterpret_cast<const Maths::GeoBounds&>(bounds);
     Maths::GeoVector2<float> corner = cBounds.getCornerInWorldSpace(index);
     return reinterpret_cast<NovelRTGeoVector2F&>(corner);
   }
 
-  bool GeoBounds_pointIsWithinBounds(const GeoBounds_t& bounds, const NovelRTGeoVector2F& point) {
+  NovelRTBool NovelRT_GeoBounds_pointIsWithinBounds(const NovelRTGeoBounds bounds, const NovelRTGeoVector2F point) {
     const Maths::GeoBounds& cBounds = reinterpret_cast<const Maths::GeoBounds&>(bounds);
     const Maths::GeoVector2<float>& cPoint = reinterpret_cast<const Maths::GeoVector2<float>&>(point);
-    return cBounds.pointIsWithinBounds(cPoint);
+    if (cBounds.pointIsWithinBounds(cPoint)) {
+      return NOVELRT_TRUE;
+    }
+    return NOVELRT_FALSE;
   }
 
-  NovelRTGeoVector2F GeoBounds_getExtents(const GeoBounds_t& bounds) {
+  NovelRTGeoVector2F NovelRT_GeoBounds_getExtents(const NovelRTGeoBounds bounds) {
     const Maths::GeoBounds cBounds = reinterpret_cast<const Maths::GeoBounds&>(bounds);
     Maths::GeoVector2<float> extents = cBounds.getExtents(); 
     return reinterpret_cast<NovelRTGeoVector2F&>(extents);
   }
 
-  bool GeoBounds_intersectsWith(const GeoBounds_t& first, const GeoBounds_t& other) {
+  NovelRTBool NovelRT_GeoBounds_intersectsWith(const NovelRTGeoBounds first, const NovelRTGeoBounds other) {
     const Maths::GeoBounds& cFirst = reinterpret_cast<const Maths::GeoBounds&>(first);
     const Maths::GeoBounds& cOther = reinterpret_cast<const Maths::GeoBounds&>(other);
-    return cFirst.intersectsWith(cOther);
+    if (cFirst.intersectsWith(cOther)) {
+      return NOVELRT_TRUE;
+    }
+    return NOVELRT_FALSE;
   }
 }
 
