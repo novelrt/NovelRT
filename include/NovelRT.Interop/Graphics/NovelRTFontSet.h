6004421b52f9907e3e125741f5be0935cc2ffaa8
Matt Jones <matthewj866@gmail.com>
2020-10-01 02:09:11 +0100

    FontSet files didn't get added in previous commit for some reason?? weird.

---
+++ include/NovelRT.Interop/Graphics/NovelRTFontSet.h
@@ -0,0 +1,23 @@
+// Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root for more information.
+
+#ifndef NOVELRT_INTEROP_GRAPHICS_FONTSET_H
+#define NOVELRT_INTEROP_GRAPHICS_FONTSET_H
+
+#include "../NovelRTTransform.h"
+#include "NovelRTRGBAConfig.h"
+#include "NovelRTGraphicsTypedefs.h"
+#include "../Maths/NovelRTGeoVector2F.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  int32_t NovelRT_FontSet_loadFontAsTextureSet(NovelRTFontSet fontSet, const char* file, float fontSize, const char** errorMessage);
+  int32_t NovelRT_FontSet_getFontFile(NovelRTFontSet fontSet, const char** outputFontFile, const char** errorMessage);
+  int32_t NovelRT_FontSet_getFontSize(NovelRTFontSet fontSet, float* outputFontSize, const char** errorMessage);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //!NOVELRT_INTEROP_GRAPHICS_FONTSET_H
