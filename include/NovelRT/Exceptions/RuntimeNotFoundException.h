8b6933fe15b4ec9fb878cf14e43d736a20d1a342
Pheubel <luca.guagliardo@live.nl>
2020-12-07 18:39:28 +0100

    Create RuntimeNotFoundException.h

---
+++ include/NovelRT/Exceptions/RuntimeNotFoundException.h
@@ -0,0 +1,19 @@
+// Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root for more information.
+
+#ifndef NOVELRT_RUNTIME_NOT_FOUND_EXCEPTION_H
+#define NOVELRT_RUNTIME_NOT_FOUND_EXCEPTION_H
+
+#ifndef NOVELRT_H
+#error Please do not include this directly. Use the centralised header (NovelRT.h) instead!
+#endif
+
+namespace NovelRT::Exceptions {
+  class RuntimeNotFoundException final : public std::runtime_error{
+    public:
+    RuntimeNotFoundException(const std::string &message)
+    : std::runtime_error(message)
+    {}
+  };
+}
+
+#endif //NOVELRT_RUNTIME_NOT_FOUND_EXCEPTION_H
