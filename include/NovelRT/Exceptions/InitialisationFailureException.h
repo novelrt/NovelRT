0a4512591a9f0e8ce3559c0504750b99510a1829
Pheubel <luca.guagliardo@live.nl>
2020-12-07 18:38:53 +0100

    Create InitialisationFailureException.h

---
+++ include/NovelRT/Exceptions/InitialisationFailureException.h
@@ -0,0 +1,25 @@
+// Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root for more information.
+
+#ifndef NOVELRT_INITIALIZATION_FAILURE_EXCEPTION_H
+#define NOVELRT_INITIALIZATION_FAILURE_EXCEPTION_H
+
+#ifndef NOVELRT_H
+#error Please do not include this directly. Use the centralised header (NovelRT.h) instead!
+#endif
+
+namespace NovelRT::Exceptions {
+  class InitialisationFailureException final : public std::runtime_error {
+  public:
+    InitialisationFailureException(const std::string &message)
+    : std::runtime_error(message)
+    {}
+    InitialisationFailureException(const std::string &message, const std::string &arguments)
+    : std::runtime_error(message + " error: " + arguments)
+    {}
+    InitialisationFailureException(const std::string &message, const int argument)
+    : InitialisationFailureException(message, std::to_string(argument))
+    {}
+  };
+} // namespace NovelRT::Exceptions
+
+#endif //NOVELRT_INITIALIZATION_FAILURE_EXCEPTION_H
