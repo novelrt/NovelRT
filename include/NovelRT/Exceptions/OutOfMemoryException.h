236bddfccceb5a2a4482992fcdd3bf76c6d301ee
Pheubel <luca.guagliardo@live.nl>
2020-12-07 18:39:25 +0100

    Create OutOfMemoryException.h

---
+++ include/NovelRT/Exceptions/OutOfMemoryException.h
@@ -0,0 +1,19 @@
+// Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root for more information.
+
+#ifndef NOVELRT_OUT_OF_MEMORY_EXCEPTION_H
+#define NOVELRT_OUT_OF_MEMORY_EXCEPTION_H
+
+#ifndef NOVELRT_H
+#error Please do not include this directly. Use the centralised header (NovelRT.h) instead!
+#endif
+
+namespace NovelRT::Exceptions {
+  class OutOfMemoryException final : public std::runtime_error{
+    public:
+    OutOfMemoryException(const std::string &message)
+    : std::runtime_error(message)
+    {}
+  };
+}
+
+#endif //NOVELRT_OUT_OF_MEMORY_EXCEPTION_H
