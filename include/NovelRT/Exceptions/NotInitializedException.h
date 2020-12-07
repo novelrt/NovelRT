2ec67d5403f55f38c836d2dba2f7442e5f5f1afc
Pheubel <luca.guagliardo@live.nl>
2020-12-07 18:38:36 +0100

    renamed ServiceNotInitialized

--- include/NovelRT/Exceptions/NotInitializedException.h
+++ include/NovelRT/Exceptions/NotInitializedException.h
@@ -1,7 +1,7 @@
 // Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root for more information.
 
-#ifndef NOVELRT_SERVICE_NOT_INITIALIZED_EXCEPTION_H
-#define NOVELRT_SERVICE_NOT_INITIALIZED_EXCEPTION_H
+#ifndef NOVELRT_SERVICE_NOT_INITIALISED_EXCEPTION_H
+#define NOVELRT_SERVICE_NOT_INITIALISED_EXCEPTION_H
 
 #ifndef NOVELRT_H
 #error Please do not include this directly. Use the centralised header (NovelRT.h) instead!
@@ -9,16 +9,16 @@
 
 namespace NovelRT::Exceptions
 {
-  class ServiceNotInitializedException final : public std::runtime_error
+  class NotInitialisedException final : public std::runtime_error
   {
   public:
-    ServiceNotInitializedException(const std::string &functionName, const std::string &message)
+    NotInitialisedException(const std::string &functionName, const std::string &message)
         : std::runtime_error("Unable to continue! Dangerous call being made to " + functionName + ". " + message)
     {}
-    ServiceNotInitializedException(const std::string &message)
+    NotInitialisedException(const std::string &message)
         : std::runtime_error("Unable to continue! " + message)
     {}
   };
 } // namespace NovelRT::Exceptions
 
-#endif //NOVELRT_SERVICE_NOT_INITIALIZED_EXCEPTION_H
+#endif //NOVELRT_SERVICE_NOT_INITIALISED_EXCEPTION_H
