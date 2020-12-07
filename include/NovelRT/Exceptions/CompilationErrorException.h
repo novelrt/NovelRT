9e369967b741e97afacad646609c913b572cac6a
Pheubel <luca.guagliardo@live.nl>
2020-12-07 18:38:15 +0100

    Create CompilationErrorException.h

---
+++ include/NovelRT/Exceptions/CompilationErrorException.h
@@ -0,0 +1,21 @@
+// Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root for more information.
+
+#ifndef NOVELRT_COMPILATION_ERROR_EXCEPTION_H
+#define NOVELRT_COMPILATION_ERROR_EXCEPTION_H
+
+#ifndef NOVELRT_H
+#error Please do not include this directly. Use the centralised header (NovelRT.h) instead!
+#endif
+
+namespace NovelRT::Exceptions
+{
+  class CompilationErrorException final : public std::runtime_error {
+  public:
+    CompilationErrorException(const std::string &fileName, const std::string &errorMessage) 
+    : std::runtime_error("Failed to compile \"" + fileName + "\". Error: " + errorMessage)
+    {}
+  };
+} // namespace NovelRT::Exceptions
+
+
+#endif //NOVELRT_COMPILATION_ERROR_EXCEPTION_H
