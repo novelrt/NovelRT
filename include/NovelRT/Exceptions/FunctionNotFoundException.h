35ee1f3ad6c9f24041af417e763dddeefb841709
Pheubel <luca.guagliardo@live.nl>
2020-12-07 18:38:50 +0100

    Create FunctionNotFoundException.h

---
+++ include/NovelRT/Exceptions/FunctionNotFoundException.h
@@ -0,0 +1,28 @@
+// Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root for more information.
+
+#ifndef NOVELRT_FUNCTION_NOT_FOUND_EXCEPTION_H
+#define NOVELRT_FUNCTION_NOT_FOUND_EXCEPTION_H
+
+#ifndef NOVELRT_H
+#error Please do not include this directly. Use the centralised header (NovelRT.h) instead!
+#endif
+
+namespace NovelRT::Exceptions {
+  class FunctionNotFoundException final : public std::runtime_error{
+    public:
+    FunctionNotFoundException(const std::string &message)
+    : std::runtime_error(message)
+    {}
+    FunctionNotFoundException(const std::string &message, int errorCode)
+    : std::runtime_error(message + "Error code:" + std::to_string(errorCode) + ".")
+    {}
+    FunctionNotFoundException(const std::string &message, const std::string &functionName)
+    : std::runtime_error(message + "Function \""+ functionName +"\".")
+    {}
+    FunctionNotFoundException(const std::string &message, const std::string &functionName, int errorCode)
+    : std::runtime_error(message + "Function \""+ functionName +"\". Error code:" + std::to_string(errorCode) + ".")
+    {}
+  };
+}
+
+#endif //NOVELRT_FUNCTION_NOT_FOUND_EXCEPTION_H
