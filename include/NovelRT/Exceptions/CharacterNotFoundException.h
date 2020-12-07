4436f3189ed63de2b98c0f5c519d994dbed62c1a
Pheubel <luca.guagliardo@live.nl>
2020-12-07 18:38:01 +0100

    Create CharacterNotFoundException.h

---
+++ include/NovelRT/Exceptions/CharacterNotFoundException.h
@@ -0,0 +1,21 @@
+// Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root for more information.
+
+#ifndef NOVELRT_CHARACTER_NOT_FOUND_EXCEPTION_H
+#define NOVELRT_CHARACTER_NOT_FOUND_EXCEPTION_H
+
+#ifndef NOVELRT_H
+#error Please do not include this directly. Use the centralised header (NovelRT.h) instead!
+#endif
+
+namespace NovelRT::Exceptions
+{
+  class CharacterNotFoundException final : public std::runtime_error{
+    public:
+    CharacterNotFoundException(const std::string &message, const char c)
+    : std::runtime_error(message +" character: " + c)
+    {}
+  };
+} // namespace NovelRT::Exceptions
+
+
+#endif //NOVELRT_CHARACTER_NOT_FOUND_EXCEPTION_H
