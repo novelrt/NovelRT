a2172c5b1caaf2a7c05fc15d1f4108cd3da6a1d0
Pheubel <luca.guagliardo@live.nl>
2020-12-20 14:06:00 +0100

    added standardised message

--- include/NovelRT/Exceptions/FunctionNotFoundException.h
+++ include/NovelRT/Exceptions/FunctionNotFoundException.h
@@ -9,17 +9,17 @@
 namespace NovelRT::Exceptions {
   class FunctionNotFoundException final : public std::runtime_error {
   public:
-    FunctionNotFoundException(const std::string &message) :
-      std::runtime_error(message)
+    FunctionNotFoundException() :
+      std::runtime_error("Failed to locate the specified managed function")
     {}
-    FunctionNotFoundException(const std::string &message, int errorCode) :
-      std::runtime_error(message + "Error code:" + std::to_string(errorCode) + ".")
+    FunctionNotFoundException(int errorCode) :
+      std::runtime_error(std::string("Failed to locate the specified managed function. Error code: ") + std::to_string(errorCode) + ".")
     {}
-    FunctionNotFoundException(const std::string &message, const std::string &functionName) :
-      std::runtime_error(message + "Function \""+ functionName +"\".")
+    FunctionNotFoundException(const std::string &functionName) :
+      std::runtime_error(std::string("Failed to locate the specified managed function. Function \"")+ functionName +"\".")
     {}
     FunctionNotFoundException(const std::string &message, const std::string &functionName, int errorCode) :
-      std::runtime_error(message + "Function \""+ functionName +"\". Error code:" + std::to_string(errorCode) + ".")
+      std::runtime_error(std::string("Failed to locate the specified managed function. Function \""+ functionName +"\". Error code: ") + std::to_string(errorCode) + ".")
     {}
   };
 }
