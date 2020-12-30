cf02cb6de132345c66f24a681ea681c52b22bf91
Ken Johnson <k.rjohnson94@outlook.com>
2020-12-30 10:26:32 -0500

    Initial working clang-format impl

---
+++ cmake/clang-format.cmake
@@ -0,0 +1,47 @@
+file(GLOB_RECURSE
+     ALL_SRC_FILES
+      src/*/*.c src/*/*.cpp include/*/*.h include/*/*.hpp
+     )
+
+
+find_program(CLANG_FORMAT "clang-format")
+if(CLANG_FORMAT)
+  message("Found clang-format at ${CLANG_FORMAT}")
+  if(WIN32)
+    add_custom_target(
+        clang-format
+        COMMAND clang-format
+        -i
+        -style=file
+        ${ALL_SRC_FILES}
+    )
+    add_custom_target(
+        clang-format-ci
+        COMMAND clang-format
+        -n
+        --output-replacements-xml
+        -style=file
+        ${ALL_SRC_FILES}
+    )
+  else()
+    add_custom_target(
+        clang-format
+        COMMAND /usr/bin/clang-format
+        -i
+        -style=file
+        ${ALL_SRC_FILES}
+    )
+    add_custom_target(
+        clang-format-ci
+        COMMAND /usr/bin/clang-format
+        -n
+        --output-replacements-xml
+        -style=file
+        ${ALL_SRC_FILES}
+    )
+  endif()
+else()
+  message("Did not find clang-format! Disabling target.")
+endif()
+
+
