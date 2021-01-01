94c36017bc09cd0f0fcb5c02a84af1063649eea9
Ken Johnson <k.rjohnson94@outlook.com>
2021-01-01 09:39:42 -0500

    Fixed scripts perms (i believe)

--- scripts/ci-checkdiff.sh
+++ scripts/ci-checkdiff.sh
@@ -1,5 +1,25 @@
-git diff > ../clang-format.patch
-if [-s clang-format.patch ]
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  ScriptRoot="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$ScriptRoot/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+ScriptRoot="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+RepoRoot="$ScriptRoot/.."
+ArtifactsDir="$RepoRoot/artifacts"
+CreateDirectory "$ArtifactsDir"
+
+function CreateDirectory {
+  if [ ! -d "$1" ]; then
+    mkdir -p "$1"
+  fi
+}
+
+#See if diff picks up any changes after running clang-format
+git diff > "$ArtifactsDir/clang-format.patch"
+
+if [-s "$ArtifactsDir/clang-format.patch" ]
 then
   raise error "Changes are required!"
 else
