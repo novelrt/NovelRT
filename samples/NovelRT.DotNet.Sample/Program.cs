d64e8ca218df301ce951ab3115c9d7b9af378f4e
Tanner Gooding <tagoo@outlook.com>
2021-03-27 19:40:22 -0700

    Adding a NovelRT.DotNet.Sample.csproj

---
+++ samples/NovelRT.DotNet.Sample/Program.cs
@@ -0,0 +1,11 @@
+// Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root for more information.
+
+namespace NovelRT.DotNet.Sample
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
