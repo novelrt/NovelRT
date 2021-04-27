9dd94f08f1a494397c8539fdae4f72898d93f253
Stathis Papadopoulos <stathisthemagic@gmail.com>
2021-04-28 01:25:47 +0300

    Inherited ShimObject

---
+++ src/NovelRT.DotNet/Interop/Ecs/SystemScheduler.cs
@@ -0,0 +1,14 @@
+// Copyright © Matt Jones and Contributors. Licensed under the MIT License (MIT). See LICENCE.md in the repository root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovelRT.Interop.Ecs
+{
+    class SystemScheduler : ShimObject
+    {
+    }
+}
---
+++ src/NovelRT.DotNet/Interop/Ecs/UnsafeComponentView.cs
@@ -0,0 +1,21 @@
+// Copyright © Matt Jones and Contributors. Licensed under the MIT License (MIT). See LICENCE.md in the repository root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovelRT.Interop.Ecs
+{
+    public class UnsafeComponentView:ShimObject
+    {
+        public UnsafeComponentView(nuint poolId, ComponentBufferMemoryContainer container)
+        {
+            internalHandle = NovelRT.Nrt_UnsafeComponentView_Create(poolId, container.internalHandle);
+        }
+        public UnsafeComponentView(nuint poolId){ throw new NotImplementedException(); }
+        public UnsafeComponentView(ComponentBufferMemoryContainer container) { throw new NotImplementedException();  }
+        public UnsafeComponentView(IntPtr viewId){ throw new NotImplementedException(); }
+    }
+}
