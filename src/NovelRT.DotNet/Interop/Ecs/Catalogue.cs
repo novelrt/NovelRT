ba1b5de24961d1205b2663d43470dc209b51b2b1
Stathis Papadopoulos <stathisthemagic@gmail.com>
2021-04-28 01:26:10 +0300

    Inherited ShimObject

--- src/NovelRT.DotNet/Interop/Ecs/Catalogue.cs
+++ src/NovelRT.DotNet/Interop/Ecs/Catalogue.cs
@@ -8,13 +8,46 @@ using System.Threading.Tasks;
 
 namespace NovelRT.Interop.Ecs
 {
-    public class Catalogue
+    public class Catalogue : ShimObject
     {
-        IntPtr pointer;
-        public Catalogue(nuint poolId, IntPtr componentCache,IntPtr entityCache)
+        public Catalogue(nuint poolId, ComponentCache componentCache, EntityCache entityCache)
         {
-            pointer=NovelRT.Nrt_Catalogue_Create(poolId, componentCache, entityCache);
+            Create(poolId, componentCache.internalHandle, entityCache.internalHandle);
         }
-
+        public Catalogue(nuint poolId, IntPtr componentCache, EntityCache entityCache){
+            Create(poolId, componentCache, entityCache.internalHandle);
+        }
+        public Catalogue(nuint poolId, ComponentCache componentCache, IntPtr entityCache)
+        {
+            Create(poolId, componentCache.internalHandle, entityCache);
+        }
+        public Catalogue(nuint poolId, IntPtr componentCache, IntPtr entityCache)
+        {
+            Create(poolId, componentCache, entityCache);
+        }
+        public void Create(nuint poolId, IntPtr componentCache, IntPtr entityCache)
+        {
+            internalHandle = NovelRT.Nrt_Catalogue_Create(poolId, componentCache, entityCache);
+        }
+        //fix capitalization, related: https://github.com/novelrt/NovelRT/issues/293
+        nuint CreateEntity()
+        {
+            return NovelRT.Nrt_catalogue_CreateEntity(internalHandle);
+        }
+        public unsafe UnsafeComponentView GetUnsafeComponentViewByID(nuint componentID)
+        {
+            IntPtr returnedView = new(0);
+            NrtResult result = NovelRT.Nrt_Catalogue_GetComponentViewById(internalHandle, componentID, &returnedView);
+            if (result == NrtResult.NRT_SUCCESS)
+            {
+                return new UnsafeComponentView(returnedView);
+            }
+            throw new ArgumentOutOfRangeException();
+        }
+        //Stathis: backwards compatibility until (safe) ComponentView is implemented.
+        UnsafeComponentView GetComponentViewByID(nuint componentId)
+        {
+            return GetUnsafeComponentViewByID(componentId);
+        }
     }
 }
