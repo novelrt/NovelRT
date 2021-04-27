4f664e91055879d70e5113e83065ae136036b184
Stathis Papadopoulos <stathisthemagic@gmail.com>
2021-04-28 01:25:32 +0300

    Created ShimObject
    ShimObject is a simple abstract class to parent all the other Ecs classes.

---
+++ src/NovelRT.DotNet/Interop/Ecs/ShimObject.cs
@@ -0,0 +1,19 @@
+// Copyright © Matt Jones and Contributors. Licensed under the MIT License (MIT). See LICENCE.md in the repository root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovelRT.Interop.Ecs
+{
+    public abstract class ShimObject
+    {
+        IntPtr _internalHandle;
+        /// <summary>
+        /// The unique handle of the ShimObject.
+        /// </summary>
+        public IntPtr internalHandle { get; internal set; }
+    }
+}
