2818e7ab1e03c54dac67ec9d8b35afb15c8d3f42
Stathis Papadopoulos <stathisthemagic@gmail.com>
2021-05-05 20:28:34 +0300

    Implemented Core methods
    Missing Sparse set related ones

--- src/NovelRT.DotNet/Ecs/ComponentBufferMemoryContainer.cs
+++ src/NovelRT.DotNet/Ecs/ComponentBufferMemoryContainer.cs
@@ -7,27 +7,68 @@ using System.Text;
 using System.Threading.Tasks;
 using static NovelRT.Interop.NovelRT;
 using NovelRT.Interop;
+using NovelRT.Utilities;
 
 namespace NovelRT.Ecs
 {
     public class ComponentBufferMemoryContainer : ShimObject
     {
-        public unsafe ComponentBufferMemoryContainer(nuint poolSize, void* deleteInstructionState, nuint sizeOfDataTypeInBytes, delegate* unmanaged<IntPtr,IntPtr, nuint,void> function){
-            
-            //internalHandle=Nrt_ComponentBufferMemoryContainer_Create
-            Nrt_ComponentBufferMemoryContainer_Create(poolSize, deleteInstructionState,sizeOfDataTypeInBytes,function);
+        public unsafe ComponentBufferMemoryContainer(nuint poolSize, void* deleteInstructionState, nuint sizeOfDataTypeInBytes, delegate* unmanaged<IntPtr, IntPtr, nuint, void> function)
+        {
+            Nrt_ComponentBufferMemoryContainer_Create(poolSize, deleteInstructionState, sizeOfDataTypeInBytes, function);
         }
         void PrepContainerForFrame(EntityIdVector entitiesToDelete)
         {
             Nrt_ComponentBufferMemoryContainer_PrepContainerForFrame(internalHandle, entitiesToDelete.internalHandle);
         }
-        IntPtr GetDeleteInstructionState(){
+        IntPtr GetDeleteInstructionState()
+        {
             return Nrt_ComponentBufferMemoryContainer_GetDeleteInstructionState(internalHandle);
         }
-        void PushComponentUpdateInstruction(nuint poolId){
-            //NrtResult result= Nrt_ComponentBufferMemoryContainer_PushComponentUpdateInstruction(internalHandle, poolId, [NativeTypeName("NrtEntityId")] nuint entity, [NativeTypeName("const void *")] void* componentData);
+        public unsafe void PushComponentUpdateInstruction<T>(nuint poolId, nuint entity, in T componentData) where T : unmanaged
+        {
+            NrtResult result;
+            fixed (void* ptr = &componentData)
+            {
+                result = Nrt_ComponentBufferMemoryContainer_PushComponentUpdateInstruction(internalHandle, poolId, entity, ptr);
+            }
+            NrtUtilities.ThrowExceptionIfFailed(result, this);
         }
-                public override void Dispose(bool isDisposing)
+        public unsafe T GetComponent<T>(nuint entity) where T : unmanaged
+        {
+            T result;
+            NrtResult nrtResult = Nrt_ComponentBufferMemoryContainer_GetComponent(this.internalHandle, entity, (IntPtr*)&result);
+            NrtUtilities.ThrowExceptionIfFailed(nrtResult, this);
+            return result;
+        }
+        public bool HasComponent(nuint entity){
+            NrtBool result=Nrt_ComponentBufferMemoryContainer_HasComponent(this.internalHandle,entity);
+            return NrtUtilities.NrtBoolToBool(result);
+        }
+        public nuint GetImmutableDataLength(){
+            return Nrt_ComponentBufferMemoryContainer_GetImmutableDataLength(this.internalHandle);
+        }
+        
+        /*
+        [DllImport("NovelRT.Interop", ExactSpelling = true)]
+        [return: NativeTypeName("NrtComponentBufferMemoryContainer_ImmutableDataViewHandle")]
+        public static extern IntPtr Nrt_ComponentBufferMemoryContainer_GetComponentUnsafe([NativeTypeName("NrtComponentBufferMemoryContainerHandle")] IntPtr container, [NativeTypeName("NrtEntityId")] nuint entity);
+        [DllImport("NovelRT.Interop", ExactSpelling = true)]
+        [return: NativeTypeName("NrtSparseSetMemoryContainer_ConstIteratorHandle")]
+        public static extern IntPtr Nrt_ComponentBufferMemoryContainer_begin([NativeTypeName("NrtComponentBufferMemoryContainerHandle")] IntPtr container);
+
+        [DllImport("NovelRT.Interop", ExactSpelling = true)]
+        [return: NativeTypeName("NrtSparseSetMemoryContainer_ConstIteratorHandle")]
+        public static extern IntPtr Nrt_ComponentBufferMemoryContainer_end([NativeTypeName("NrtComponentBufferMemoryContainerHandle")] IntPtr container);
+
+        [DllImport("NovelRT.Interop", ExactSpelling = true)]
+        [return: NativeTypeName("const void *")]
+        public static extern void* Nrt_ComponentBufferMemoryContainer_ImmutableDataView_GetDataHandle([NativeTypeName("NrtComponentBufferMemoryContainer_ImmutableDataViewHandle")] IntPtr view);
+
+        [DllImport("NovelRT.Interop", ExactSpelling = true)]
+        public static extern NrtResult Nrt_ComponentBufferMemoryContainer_ImmutableDataView_Destroy([NativeTypeName("NrtComponentBufferMemoryContainer_ImmutableDataViewHandle")] IntPtr view);
+        */
+        public override void Dispose(bool isDisposing)
         {
             if (internalHandle != IntPtr.Zero)
             {
@@ -38,41 +79,5 @@ namespace NovelRT.Ecs
                 }
             }
         }
-        /*
-        [DllImport("NovelRT.Interop", ExactSpelling = true)]
-        public static extern NrtResult Nrt_ComponentBufferMemoryContainer_PushComponentUpdateInstruction([NativeTypeName("NrtComponentBufferMemoryContainerHandle")] IntPtr container, [NativeTypeName("size_t")] nuint poolId, [NativeTypeName("NrtEntityId")] nuint entity, [NativeTypeName("const void *")] void* componentData);
-
-        [DllImport("NovelRT.Interop", ExactSpelling = true)]
-        public static extern NrtResult Nrt_ComponentBufferMemoryContainer_GetComponent([NativeTypeName("NrtComponentBufferMemoryContainerHandle")] IntPtr container, [NativeTypeName("NrtEntityId")] nuint entity, [NativeTypeName("NrtComponentBufferMemoryContainer_ImmutableDataViewHandle *")] IntPtr* outputResult);
-
-        [DllImport("NovelRT.Interop", ExactSpelling = true)]
-        [return: NativeTypeName("NrtComponentBufferMemoryContainer_ImmutableDataViewHandle")]
-        public static extern IntPtr Nrt_ComponentBufferMemoryContainer_GetComponentUnsafe([NativeTypeName("NrtComponentBufferMemoryContainerHandle")] IntPtr container, [NativeTypeName("NrtEntityId")] nuint entity);
-
-        [DllImport("NovelRT.Interop", ExactSpelling = true)]
-        public static extern NrtBool Nrt_ComponentBufferMemoryContainer_HasComponent([NativeTypeName("NrtComponentBufferMemoryContainerHandle")] IntPtr container, [NativeTypeName("NrtEntityId")] nuint entity);
-
-        [DllImport("NovelRT.Interop", ExactSpelling = true)]
-        [return: NativeTypeName("size_t")]
-        public static extern nuint Nrt_ComponentBufferMemoryContainer_GetImmutableDataLength([NativeTypeName("NrtComponentBufferMemoryContainerHandle")] IntPtr container);
-
-        [DllImport("NovelRT.Interop", ExactSpelling = true)]
-        [return: NativeTypeName("NrtSparseSetMemoryContainer_ConstIteratorHandle")]
-        public static extern IntPtr Nrt_ComponentBufferMemoryContainer_begin([NativeTypeName("NrtComponentBufferMemoryContainerHandle")] IntPtr container);
-
-        [DllImport("NovelRT.Interop", ExactSpelling = true)]
-        [return: NativeTypeName("NrtSparseSetMemoryContainer_ConstIteratorHandle")]
-        public static extern IntPtr Nrt_ComponentBufferMemoryContainer_end([NativeTypeName("NrtComponentBufferMemoryContainerHandle")] IntPtr container);
-
-        [DllImport("NovelRT.Interop", ExactSpelling = true)]
-        public static extern NrtResult Nrt_ComponentBufferMemoryContainer_Destroy([NativeTypeName("NrtComponentBufferMemoryContainerHandle")] IntPtr container);
-
-        [DllImport("NovelRT.Interop", ExactSpelling = true)]
-        [return: NativeTypeName("const void *")]
-        public static extern void* Nrt_ComponentBufferMemoryContainer_ImmutableDataView_GetDataHandle([NativeTypeName("NrtComponentBufferMemoryContainer_ImmutableDataViewHandle")] IntPtr view);
-
-        [DllImport("NovelRT.Interop", ExactSpelling = true)]
-        public static extern NrtResult Nrt_ComponentBufferMemoryContainer_ImmutableDataView_Destroy([NativeTypeName("NrtComponentBufferMemoryContainer_ImmutableDataViewHandle")] IntPtr view);
-        */
     }
 }
