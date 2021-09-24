a6eb7454b4c76cb1db96ab79ecb41e353241d3e9
Matt Jones <matthewj866@gmail.com>
2021-09-24 10:50:39 +0100

    Ubuntu please.

--- include/NovelRT/Experimental/Graphics/IGraphicsMemoryRegionCollection.h
+++ include/NovelRT/Experimental/Graphics/IGraphicsMemoryRegionCollection.h
@@ -286,7 +286,7 @@ namespace NovelRT::Experimental::Graphics
                 size_t calculatedSize = 0ULL;
                 size_t calculatedTotalFreeRegionSize = 0ULL;
 
-                size_t calculatedFreeRegionCount = 0;
+                int32_t calculatedFreeRegionCount = 0;
                 size_t calculatedFreeRegionsToRegisterCount = 0;
 
                 bool isPreviousRegionFree = false;
