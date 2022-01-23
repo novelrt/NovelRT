3bb3dbb0e00c5494475c3937daddd4327868a268
Matt Jones <matthewj866@gmail.com>
2022-01-23 01:08:10 +0000

    Remove GLAD. Update dependency list. Listen to rules of nature as I continue to delete things.

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,6 @@ class NovelRTConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = [
         ("freetype/2.10.1"),
-        ("glad/0.1.33"),
         ("glfw/3.3.2"),
         ("glm/0.9.9.7"),
         ("gtest/1.10.0"),
