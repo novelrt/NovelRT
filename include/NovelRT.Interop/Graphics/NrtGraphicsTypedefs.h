b14284afff470ca0a9455993d08adc50468e1688
SmylEFace <anthonyidiaz32@gmail.com>
2022-02-24 12:14:47 -0500

    datatype fix

--- include/NovelRT.Interop/Graphics/NrtGraphicsTypedefs.h
+++ include/NovelRT.Interop/Graphics/NrtGraphicsTypedefs.h
@@ -1,7 +1,7 @@
 typedef struct
 {
-    int32_t r;
-    int32_t g;
-    int32_t b;
-    int32_t a;
+    uint8_t r; //these still need to be uint
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
 } NrtRGBAColour;
