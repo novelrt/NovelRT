0c5bc28177b86004f004b6175add35e815624704
SmylEFace <anthonyidiaz32@gmail.com>
2022-02-24 11:52:23 -0500

    NRTColour requested changes

--- src/NovelRT.Interop/Graphics/NrtRGBAColour.cpp
+++ src/NovelRT.Interop/Graphics/NrtRGBAColour.cpp
@@ -16,26 +16,26 @@ extern "C"
 
     float Nrt_RGBAColour_getRScalar(NrtRGBAColour colourConfig)
     {
-        RGBAColour* colourConfigPtr = reinterpret_cast<RGBAColour*>(&colourConfig);
-        return colourConfigPtr->getRScalar();
+        RGBAColour colourConfigPtr = *reinterpret_cast<RGBAColour*>(&colourConfig);
+        return colourConfigPtr.getRScalar();
     }
 
     float Nrt_RGBAColour_getGScalar(NrtRGBAColour colourConfig)
     {
-        RGBAColour* colourConfigPtr = reinterpret_cast<RGBAColour*>(&colourConfig);
-        return colourConfigPtr->getGScalar();
+        RGBAColour colourConfigPtr = *reinterpret_cast<RGBAColour*>(&colourConfig);
+        return colourConfigPtr.getGScalar();
     }
 
     float Nrt_RGBAColour_getBScalar(NrtRGBAColour colourConfig)
     {
-        RGBAColour* colourConfigPtr = reinterpret_cast<RGBAColour*>(&colourConfig);
-        return colourConfigPtr->getBScalar();
+        RGBAColour colourConfigPtr = *reinterpret_cast<RGBAColour*>(&colourConfig);
+        return colourConfigPtr.getBScalar();
     }
 
     float Nrt_RGBAColour_getAScalar(NrtRGBAColour colourConfig)
     {
-        RGBAColour* colourConfigPtr = reinterpret_cast<RGBAColour*>(&colourConfig);
-        return colourConfigPtr->getAScalar();
+        RGBAColour colourConfigPtr = *reinterpret_cast<RGBAColour*>(&colourConfig);
+        return colourConfigPtr.getAScalar();
     }
 
 #ifdef __cplusplus
