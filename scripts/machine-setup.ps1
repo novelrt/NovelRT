22873e33ed780b68ec35d94f80141529177d6964
Ken Johnson <k.rjohnson94@outlook.com>
2022-04-18 00:22:33 -0400

    Removed dotnet install and upgraded python in machine-setup for Windows

--- scripts/machine-setup.ps1
+++ scripts/machine-setup.ps1
@@ -1,13 +1,12 @@
 # NOTE: DO NOT RUN THIS ON DEVELOPMENT MACHINES!
 # This is intended for setting up CI machines with the correct dependencies.
 
-$DotNetInstallDirectory = Join-Path -Path $HOME -ChildPath "dotnet"
 $ProgressPreference = 'SilentlyContinue'
 
 if (!(Get-Command python -ErrorAction SilentlyContinue ))
 {
   $PythonInstaller = New-TemporaryFile | Rename-Item -NewName { $_.Name + ".exe" } -PassThru
-  Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.8.3/python-3.8.3.exe" -OutFile $PythonInstaller
+  Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe" -OutFile $PythonInstaller
 
   & $PythonInstaller /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
 
@@ -21,27 +20,6 @@ if (!(Get-Command python -ErrorAction SilentlyContinue ))
 
 if ($LastExitCode -ne 0) {
   throw "'pip install' failed"
-}
-
-$env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
-$env:DOTNET_MULTILEVEL_LOOKUP = 0
-$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = 1
-
-$DotNetInstallScript = New-TemporaryFile | Rename-Item -NewName { $_.Name + ".ps1" } -PassThru
-$DotNetExe = Join-Path -Path $DotNetInstallDirectory -ChildPath "dotnet.exe"
-Invoke-WebRequest -Uri "https://dot.net/v1/dotnet-install.ps1" -OutFile $DotNetInstallScript -UseBasicParsing
-New-Item -Path $DotNetInstallDirectory -Force -ItemType "Directory" | Out-Null
-
-& $DotNetInstallScript -Channel 6.0 -Version latest -InstallDir $DotNetInstallDirectory
-
-if ($LastExitCode -ne 0) {
-  throw "'dotnet-install' failed"
-}
-
-& $DotNetExe --info
-
-if ($LastExitCode -ne 0) {
-  throw "'dotnet' failed"
 }
 
 & cmake --version
