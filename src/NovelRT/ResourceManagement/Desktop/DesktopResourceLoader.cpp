6c846aea19205c1058d8aaee823df89a2879cdcb
Matt Jones <matthewj866@gmail.com>
2022-05-10 19:19:30 +0100

    clang-format is a memelord.

--- src/NovelRT/ResourceManagement/Desktop/DesktopResourceLoader.cpp
+++ src/NovelRT/ResourceManagement/Desktop/DesktopResourceLoader.cpp
@@ -1,7 +1,6 @@
 // Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root
 // for more information.
 
-#include "NovelRT/ResourceManagement/Desktop/DesktopResourceLoader.h"
 #include <NovelRT/ResourceManagement/Desktop/ResourceManagement.Desktop.h>
 
 namespace NovelRT::ResourceManagement::Desktop
