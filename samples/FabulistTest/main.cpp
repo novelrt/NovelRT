4784af31924c6b3aef2e0de0c26c8c3744bf262a
Matt Jones <matthewj866@gmail.com>
2022-05-11 23:10:01 +0100

    Add Fabulist integration.

---
+++ samples/FabulistTest/main.cpp
@@ -0,0 +1,83 @@
+// Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root
+// for more information.
+
+#include <NovelRT/NovelRT.h>
+
+using namespace NovelRT::Ecs;
+using namespace NovelRT::PluginManagement;
+
+NovelRT::Utilities::Event<NovelRT::Timing::Timestamp> DummyUpdateStuff;
+
+int main()
+{
+    std::cerr << "Fabulist runtime " << fabulist::runtime::get_version_string() << "\n";
+    DefaultPluginSelector selector;
+    auto windowingProvider = selector.GetDefaultPluginTypeOnCurrentPlatformFor<IWindowingPluginProvider>();
+
+    auto scheduler =
+        Configurator()
+            .WithDefaultSystemsAndComponents()
+            .WithPluginProvider(selector.GetDefaultPluginTypeOnCurrentPlatformFor<IGraphicsPluginProvider>())
+            .WithPluginProvider(windowingProvider)
+            .WithPluginProvider(selector.GetDefaultPluginTypeOnCurrentPlatformFor<IResourceManagementPluginProvider>())
+            .WithPluginProvider(selector.GetDefaultPluginTypeOnCurrentPlatformFor<IInputPluginProvider>())
+            .InitialiseAndRegisterComponents();
+
+    std::shared_ptr<NovelRT::Ecs::Graphics::DefaultRenderingSystem> renderingSystem =
+        scheduler.GetRegisteredIEcsSystemAs<NovelRT::Ecs::Graphics::DefaultRenderingSystem>();
+
+    NovelRT::Threading::FutureResult<NovelRT::Ecs::Graphics::TextureInfo> textureFuture =
+        renderingSystem->GetOrLoadTexture("novel-chan");
+
+    renderingSystem->ForceVertexTextureFutureResolution();
+
+    auto transformBuffer = scheduler.GetComponentCache().GetComponentBuffer<TransformComponent>();
+    auto entityGraphBuffer = scheduler.GetComponentCache().GetComponentBuffer<EntityGraphComponent>();
+
+    EntityId parentEntity =
+        renderingSystem->CreateSpriteEntityOutsideOfSystem(textureFuture.GetBackingConcurrentSharedPtr(), scheduler);
+
+    EntityId childEntity =
+        renderingSystem->CreateSpriteEntityOutsideOfSystem(textureFuture.GetBackingConcurrentSharedPtr(), scheduler);
+
+    EntityId childOfChildEntity =
+        renderingSystem->CreateSpriteEntityOutsideOfSystem(textureFuture.GetBackingConcurrentSharedPtr(), scheduler);
+
+    transformBuffer.PushComponentUpdateInstruction(
+        0, childEntity,
+        TransformComponent{NovelRT::Maths::GeoVector3F(200, 200, 0), NovelRT::Maths::GeoVector2F::zero(), 0});
+    transformBuffer.PushComponentUpdateInstruction(
+        0, childOfChildEntity,
+        TransformComponent{NovelRT::Maths::GeoVector3F(200, 200, 0), NovelRT::Maths::GeoVector2F::zero(), 0});
+    entityGraphBuffer.PushComponentUpdateInstruction(0, childEntity, EntityGraphComponent{true, parentEntity, 0});
+    entityGraphBuffer.PushComponentUpdateInstruction(0, childOfChildEntity, EntityGraphComponent{true, childEntity, 0});
+
+    scheduler.RegisterSystem([](auto delta, auto catalogue) {
+        ComponentView<TransformComponent> transforms = catalogue.template GetComponentView<TransformComponent>();
+
+        for (auto [entity, transform] : transforms)
+        {
+            TransformComponent newComponent{};
+            newComponent.rotationInRadians = NovelRT::Maths::Utilities::DegreesToRadians(20 * delta.getSecondsFloat());
+            newComponent.scale = NovelRT::Maths::GeoVector2F::zero();
+            transforms.PushComponentUpdateInstruction(entity, newComponent);
+        }
+    });
+
+    scheduler.GetComponentCache().PrepAllBuffersForNextFrame(std::vector<EntityId>{});
+
+    DummyUpdateStuff += [&](auto delta) { scheduler.ExecuteIteration(delta); };
+
+    NovelRT::Timing::StepTimer timer;
+
+    auto windowPtr = windowingProvider->GetWindowingDevice();
+    windowPtr->SetWindowTitle("ECS Test");
+
+    while (!windowPtr->GetShouldClose())
+    {
+        windowPtr->ProcessAllMessages();
+        timer.tick(DummyUpdateStuff);
+    }
+
+    return 0;
+}
