f7cf13aef8ec7c2c2a872b159e7a2d65d0d6dfad
Matt Jones <matthewj866@gmail.com>
2022-06-09 21:26:23 +0100

    [NOT WORKING] GLM headers have gone walkabout.

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,6 @@ class NovelRTConan(ConanFile):
     requires = [
         ("freetype/2.10.1"),
         ("glfw/3.3.6"),
-        ("glm/0.9.9.7"),
         ("gtest/1.10.0"),
         ("libsndfile/1.0.30"),
         ("ms-gsl/3.1.0"),
