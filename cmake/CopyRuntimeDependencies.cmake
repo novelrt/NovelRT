0a66290ae776d05f83cc0cf2e970c03b35818fb2
Ken Johnson <k.rjohnson94@outlook.com>
2022-09-18 00:07:16 -0400

    Linux-specific fixes

--- cmake/CopyRuntimeDependencies.cmake
+++ cmake/CopyRuntimeDependencies.cmake
@@ -19,10 +19,12 @@ function(copy_runtime_dependencies TARGET)
       )
     endif()
   endforeach()
-  add_custom_command(
-    TARGET ${TARGET} POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different
-            $<TARGET_RUNTIME_DLLS:${TARGET}> $<TARGET_FILE_DIR:${TARGET}>
-    COMMAND_EXPAND_LISTS
-  )
+  if(WIN32)
+    add_custom_command(
+      TARGET ${TARGET} POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy_if_different
+              $<TARGET_RUNTIME_DLLS:${TARGET}> $<TARGET_FILE_DIR:${TARGET}>
+      COMMAND_EXPAND_LISTS
+    )
+  endif()
 endfunction()
