24a77bf60aec089e516da054c9c9439e515092f3
Bastien JAUNY <bastien.jauny@gmail.com>
2022-10-14 10:28:26 +0200

    Used size_t limits for test variables initialization. Fixed TODO tests.

--- tests/NovelRT.Tests/Interop/Maths/QuadTreePointTest.cpp
+++ tests/NovelRT.Tests/Interop/Maths/QuadTreePointTest.cpp
@@ -1,6 +1,7 @@
 // Copyright © Matt Jones and Contributors. Licensed under the MIT License (MIT). See LICENCE.md in the repository root
 // for more information.
 
+#include <NovelRT.Interop/NrtErrorHandling.h>
 #include <NovelRT.Interop/Maths/NrtGeoVector2F.h>
 #include <NovelRT.Interop/Maths/NrtQuadTreePoint.h>
 #include <NovelRT/NovelRT.h>
@@ -28,18 +29,20 @@ TEST(InteropQuadTreePointTest, deleteReturnsSuccess)
 
 TEST(InteropQuadTreePointTest, deleteReturnsNullInstanceFailureWhenGivenNullptr)
 {
+    const char* outputError = "Unable to continue. A null instance was provided when an instance was expected.";
     ASSERT_EQ(Nrt_QuadTreePoint_delete(nullptr), NRT_FAILURE_NULL_INSTANCE_PROVIDED);
-    // EXPECT_EQ(outputError, Nrt_getErrMsgIsNullptr()); //TODO: fix this
+    EXPECT_STREQ(outputError, Nrt_getLastError());
 }
 
 TEST(InteropQuadTreePointTest,
      deleteReturnsAlreadyDeletedOrRemovedWhenPointIsBeingHeldOntoElsewhereAndHasAlreadyBeenDeletedFromCache)
 {
+    const char* outputError = "Unable to continue. The specific item has already been deleted or removed.";
     auto ptr =
         reinterpret_cast<Maths::QuadTreePoint*>(Nrt_QuadTreePoint_createFromFloat(1.0f, 1.0f))->shared_from_this();
     ASSERT_EQ(Nrt_QuadTreePoint_delete(reinterpret_cast<NrtQuadTreePointHandle>(ptr.get())), NRT_SUCCESS);
     ASSERT_EQ(Nrt_QuadTreePoint_delete(reinterpret_cast<NrtQuadTreePointHandle>(ptr.get())),
               NRT_FAILURE_ALREADY_DELETED_OR_REMOVED);
 
-    // EXPECT_EQ(outputError, Nrt_getErrMsgIsAlreadyDeletedOrRemoved()); //TODO: fix this
+    EXPECT_STREQ(outputError, Nrt_getLastError());
 }
