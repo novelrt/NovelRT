3621ddf495011ecf0bcc4b26327d5235e1a978ca
Ken Johnson <k.rjohnson94@outlook.com>
2022-11-12 06:54:09 -0500

    Corrected script

--- scripts/ci-apple-installVulkanSDK.sh
+++ scripts/ci-apple-installVulkanSDK.sh
@@ -10,27 +10,27 @@ else
     then
         echo "No destination directory passed."
     else
-        local version=$1
-        local sdk_path=$2
-        local url=https://sdk.lunarg.com/sdk/download/$version/mac/vulkansdk-macos-$version.dmg?Human=true
-        echo "Downloading Vulkan SDK v. $version"
+        version=$1
+        sdk_path=$2
+        url=https://sdk.lunarg.com/sdk/download/$version/mac/vulkansdk-macos-$version.dmg?Human=true
+        echo "Downloading Vulkan SDK v.$version"
 
         #Download the SDK
-        curl -s -L -o vulkan_sdk.dmg $url
+        curl -L -o $sdk_path/vulkan_sdk.dmg $url
 
         #Mount the dmg
-        test -f vulkan_sdk.dmg
-        local mountpoint=$(hdiutil attach vulkan_sdk.dmg | grep vulkansdk | awk 'END {print $NF}')
+        test -f $sdk_path/vulkan_sdk.dmg
+        mountpoint=$(hdiutil attach $sdk_path/vulkan_sdk.dmg | grep -o "\S.*vulkansdk-.*" | awk 'END {print $NF}')
         if [[ -d $mountpoint ]] ; then
-            echo "Mounted dmg image: 'vulkan_sdk.dmg' (mountpoint=$mountpoint)" >&2
+            echo "Mounted dmg image: '$sdk_path/vulkan_sdk.dmg' (mountpoint=$mountpoint)" >&2
         else
-            echo "Could not mount dmg image: vulkan_sdk.dmg (mountpoint=$mountpoint)" >&2
+            echo "Could not mount dmg image: $sdk_path/vulkan_sdk.dmg (mountpoint=$mountpoint)" >&2
             exit 7
         fi
 
         sudo $mountpoint/InstallVulkan.app/Contents/MacOS/InstallVulkan --root "$sdk_path" --accept-licenses --default-answer --confirm-command install
         hdiutil detach $mountpoint
-        rm vulkan_sdk.dmg
+        rm $sdk_path/vulkan_sdk.dmg
 
         #Write the VULKAN_SDK env var to the Github Environment variables
         echo "VULKAN_SDK=$sdk_path/$version/macOS" >> $GITHUB_ENV
