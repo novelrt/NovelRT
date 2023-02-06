686ddc2c33f263fd3ed751d7eaa9867d79858eac
Pheubel <luca.guagliardo@live.nl>
2023-02-06 17:23:09 +0100

    expanded joint2d base

--- include/NovelRT/Physics/Joint2D.h
+++ include/NovelRT/Physics/Joint2D.h
@@ -12,7 +12,64 @@ namespace NovelRT::Physics
 {
     class Joint2D
     {
+    protected:
+        RigidBody2D* _primaryBody;
+        RigidBody2D* _secondaryBody;
+        NovelRT::Maths::GeoVector2F _primaryOffset;
+        NovelRT::Maths::GeoVector2F _secondaryOffset;
+        float _breakForce;
+        float _breakTorque;
+        bool _collideConnectedBodies;
 
+    public:
+        [[nodiscard]] inline RigidBody2D* const GetPrimaryBody() const noexcept
+        {
+            return _primaryBody;
+        }
+
+        [[nodiscard]] inline RigidBody2D* const GetSecondaryBody() const noexcept
+        {
+            return _secondaryBody;
+        }
+        
+        [[nodiscard]] inline NovelRT::Maths::GeoVector2F GetPrimaryOffset() const noexcept
+        {
+            return _primaryOffset;
+        }
+
+        [[nodiscard]] inline NovelRT::Maths::GeoVector2F GetSecondaryOffset() const noexcept
+        {
+            return _secondaryOffset;
+        }
+
+        [[nodiscard]] inline float GetBreakForce() const noexcept
+        {
+            return _breakForce;
+        }
+
+        [[nodiscard]] inline float GetBreakTorque() const noexcept
+        {
+            return _breakTorque;
+        }
+
+        [[nodiscard]] inline bool GetCollideConnectedBodies() const noexcept
+        {
+            return _collideConnectedBodies;
+        }
+
+        virtual void SetPrimaryBody(RigidBody2D* body) = 0;
+
+        virtual void SetSecondaryBody(RigidBody2D* body) = 0;
+
+        virtual void SetPrimaryOffset(NovelRT::Maths::GeoVector2F offset) = 0;
+
+        virtual void SetSecondaryOffset(NovelRT::Maths::GeoVector2F offset) = 0;
+
+        virtual void SetBreakForce(float required) = 0;
+
+        virtual void SetBreakTorque(float required) = 0;
+
+        virtual void SetCollideConnectedBodies(bool shouldCollide) = 0;
     };
 }
 
