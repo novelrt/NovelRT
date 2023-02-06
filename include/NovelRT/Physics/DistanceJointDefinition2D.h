a043abca6f23c006bd4007273d5dd6eeb0c0da7c
Pheubel <luca.guagliardo@live.nl>
2023-02-06 13:44:23 +0100

    Create DistanceJointDefinition2D.h

---
+++ include/NovelRT/Physics/DistanceJointDefinition2D.h
@@ -0,0 +1,19 @@
+// Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root
+// for more information.
+
+#ifndef NOVELRT_PHYSICS_DISTANCEJOINTDEFINITION2D_H
+#define NOVELRT_PHYSICS_DISTANCEJOINTDEFINITION2D_H
+
+#ifndef NOVELRT_PHYSICS_H
+#error NovelRT does not support including types explicitly by default. Please include Physics.h instead for the Physics namespace subset.
+#endif
+
+namespace NovelRT::Physics
+{
+    struct DistanceJointDefinition2D final : public JointDefinition2D
+    {
+
+    };
+}
+
+#endif // !NOVELRT_PHYSICS_DISTANCEJOINTDEFINITION2D_H
