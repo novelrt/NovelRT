fcf3384845b04a9d1cf64e718fca811b7e3e2993
Pheubel <luca.guagliardo@live.nl>
2023-02-06 15:52:00 +0100

    expanded physics header

--- include/NovelRT/Physics/Physics.h
+++ include/NovelRT/Physics/Physics.h
@@ -22,10 +22,16 @@ namespace NovelRT::Physics
     class RigidBody2D;
     struct BodyDefinition2D;
     class Joint2D;
+    class DistanceJoint2D;
+    class GearJoint2D;
     class FixedJoint2D;
     class HingeJoint2D;
+    class SpringJoint2D;
     struct JointDefinition2D;
     struct DistanceJointDefinition2D;
+    struct FixedJointDefinition2D;
+    struct HingeJointDefinition2D;
+    struct SpringJointDefinition2D;
 }
 
 #include "BodyType.h"
@@ -36,11 +42,16 @@ namespace NovelRT::Physics
 #include "RigidBody2D.h"
 #include "BodyDefinition2D.h"
 #include "Joint2D.h"
+#include "DistanceJoint2D.h"
 #include "FixedJoint2D.h"
+#include "GearJoint2D.h"
 #include "HingeJoint2D.h"
+#include "SpringJoint2D.h"
 #include "JointDefinition2D.h"
 #include "DistanceJointDefinition2D.h"
 #include "FixedJointDefinition2D.h"
+#include "HingeJointDefinition2D.h"
+#include "SpringJointDefinition2D.h"
 
 
 #endif // !NOVELRT_PHYSICS_H
