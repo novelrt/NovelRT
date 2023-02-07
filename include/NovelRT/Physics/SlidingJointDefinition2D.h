1df17b56e26d1c2ccc134f877946937156f95561
Pheubel <luca.guagliardo@live.nl>
2023-02-07 13:01:32 +0100

    set up shells for spring joint

--- include/NovelRT/Physics/SlidingJointDefinition2D.h
+++ include/NovelRT/Physics/SlidingJointDefinition2D.h
@@ -1,8 +1,8 @@
 // Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root
 // for more information.
 
-#ifndef NOVELRT_PHYSICS_SPRINGJOINT2D_H
-#define NOVELRT_PHYSICS_SPRINGJOINT2D_H
+#ifndef NOVELRT_PHYSICS_SLIDINGJOINTDEFINITION2D_H
+#define NOVELRT_PHYSICS_SLIDINGJOINTDEFINITION2D_H
 
 #ifndef NOVELRT_PHYSICS_H
 #error NovelRT does not support including types explicitly by default. Please include Physics.h instead for the Physics namespace subset.
@@ -10,10 +10,10 @@
 
 namespace NovelRT::Physics
 {
-    class SpringJoint2D : public Joint2D 
+    struct SlidingJointDefinition2D final : public JointDefinition2D
     {
-
+        
     };
 }
 
-#endif // !NOVELRT_PHYSICS_SPRINGJOINT2D_H
+#endif // !NOVELRT_PHYSICS_SLIDINGJOINTDEFINITION2D_H
