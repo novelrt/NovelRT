57ef0d28abb7964bf854a2bbc3084dd85d42e02b
Pheubel <luca.guagliardo@live.nl>
2023-03-15 23:55:25 +0100

    added missing preprocessor guard

--- include/NovelRT/Physics/2D/RigidBody2D.h
+++ include/NovelRT/Physics/2D/RigidBody2D.h
@@ -4,6 +4,10 @@
 #ifndef NOVELRT_PHYSICS_PHYSICS2D_RIGIDBODY2D_H
 #define NOVELRT_PHYSICS_PHYSICS2D_RIGIDBODY2D_H
 
+#ifndef NOVELRT_PHYSICS_H
+#error NovelRT does not support including types explicitly by default. Please include Physics.h instead for the Physics namespace subset.
+#endif
+
 namespace NovelRT::Physics::Physics2D
 {
     class RigidBody2D
