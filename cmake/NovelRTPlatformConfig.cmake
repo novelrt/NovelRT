f38959683e74a41538070d5f653e4bceded0dab3
Ken Johnson <k.rjohnson94@outlook.com>
2023-06-20 21:26:09 -0400

    Switched from magic nums to STREQUAL checks

--- cmake/NovelRTPlatformConfig.cmake
+++ cmake/NovelRTPlatformConfig.cmake
@@ -3,12 +3,11 @@ set(PLATFORMS_DIR "${CMAKE_CURRENT_SOURCE_DIR}/cmake/platforms")
 if(NOT NOVELRT_TARGET IN_LIST NOVELRT_TARGET_OPTIONS)
   message(FATAL_ERROR "Could not identify a proper target to build NovelRT for!")
 else()
-  list(FIND NOVELRT_TARGET_OPTIONS ${NOVELRT_TARGET} index)
-  if(index EQUAL 0) # "Win32"
+  if(${NOVELRT_TARGET} STREQUAL "Win32")
     include(${PLATFORMS_DIR}/windows.cmake)
-  elseif(index EQUAL 1) # "Linux"
+  elseif(${NOVELRT_TARGET} STREQUAL "Linux")
     include(${PLATFORMS_DIR}/linux.cmake)
-  elseif(index EQUAL 2) # "macOS"
+  elseif(${NOVELRT_TARGET} STREQUAL "macOS")
     include(${PLATFORMS_DIR}/macos.cmake)
   endif()
 endif()
