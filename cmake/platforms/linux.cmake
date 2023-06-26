128ad3a4e0f5d119a8d221de5ccade2911889466
FiniteReality <digpoealternate@gmail.com>
2023-06-27 00:38:11 +0100

    fix: Linux builds now succeed

--- cmake/platforms/linux.cmake
+++ cmake/platforms/linux.cmake
@@ -1,7 +1,5 @@
 message(STATUS "Building NovelRT for Linux")
 
-include(NovelRTBuildDeps)
-
 #
 # Platform-specific changes
 #
@@ -17,27 +15,7 @@ endif()
 #
 # Dependency checks
 #
-find_package(spdlog ${NOVELRT_SPDLOG_VERSION} REQUIRED)
-find_package(TBB ${NOVELRT_ONETBB_VERSION} CONFIG REQUIRED)
-find_package(Microsoft.GSL ${NOVELRT_GSL_VERSION} REQUIRED)
-find_package(stduuid CONFIG REQUIRED)
-find_package(glfw3 ${NOVELRT_GLFW_VERSION} CONFIG REQUIRED)
-find_package(glm REQUIRED)
-find_package(Ogg ${NOVELRT_OGG_VERSION} REQUIRED)
-find_package(OpenAL CONFIG REQUIRED)
-find_package(Opus REQUIRED)
-find_package(Vorbis ${NOVELRT_VORBIS_VERSION} REQUIRED)
-find_package(flac ${NOVELRT_FLAC_VERSION} REQUIRED)
-find_package(SndFile REQUIRED)
-find_package(nlohmann_json REQUIRED)
 find_package(Vulkan ${NOVELRT_VULKAN_VERSION} REQUIRED)
-if(NOVELRT_DEPS_INSTALLED)
-  find_package(ZLIB CONFIG REQUIRED)
-  include(${NOVELRT_DEPS_PATH}/lib/libpng/libpng16.cmake)
-else()
-  find_package(ZLIB REQUIRED)
-  find_package(PNG ${NOVELRT_PNG_VERSION} REQUIRED)
-endif()
 
 #
 # Libraries to be linked
