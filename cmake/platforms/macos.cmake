4b560ace93852d3816b1933e086a436c651cdbf8
FiniteReality <digpoealternate@gmail.com>
2023-06-27 00:55:36 +0100

    fix: mac changes, add missing clang option

--- cmake/platforms/macos.cmake
+++ cmake/platforms/macos.cmake
@@ -1,7 +1,5 @@
 message(STATUS "Building NovelRT for macOS")
 
-include(NovelRTBuildDeps)
-
 #
 # Platform-specific changes
 #
@@ -19,28 +17,8 @@ endif("${isSystemDir}" STREQUAL "-1")
 #
 # Dependency checks
 #
-find_package(spdlog ${NOVELRT_SPDLOG_VERSION} REQUIRED)
-find_package(TBB ${NOVELRT_ONETBB_VERSION} CONFIG REQUIRED)
-find_package(Microsoft.GSL ${NOVELRT_GSL_VERSION} REQUIRED)
-find_package(stduuid CONFIG REQUIRED)
-find_package(glfw3 ${NOVELRT_GLFW_VERSION} CONFIG REQUIRED)
-find_package(glm REQUIRED)
-find_package(Ogg ${NOVELRT_OGG_VERSION} REQUIRED)
-find_package(OpenAL CONFIG REQUIRED)
-find_package(Opus REQUIRED)
-find_package(Vorbis ${NOVELRT_VORBIS_VERSION} REQUIRED)
-find_package(flac ${NOVELRT_FLAC_VERSION} REQUIRED)
-find_package(SndFile REQUIRED)
-find_package(nlohmann_json REQUIRED)
 find_package(Vulkan ${NOVELRT_VULKAN_VERSION} REQUIRED COMPONENTS MoltenVK)
 find_library(COREFOUNDATION CoreFoundation "/")
-if(NOVELRT_DEPS_INSTALLED)
-  find_package(ZLIB CONFIG REQUIRED)
-  include(${NOVELRT_DEPS_PATH}/lib/libpng/libpng16.cmake)
-else()
-  find_package(ZLIB REQUIRED)
-  find_package(PNG ${NOVELRT_PNG_VERSION} REQUIRED)
-endif()
 
 #
 # Libraries to be linked
@@ -82,10 +60,6 @@ list(APPEND NOVELRT_TARGET_COMPILE_OPTIONS
 #
 # Any verbose behaviour changes
 #
-if(NOVELRT_VERBOSE_BUILD)
-  set(CMAKE_VERBOSE_MAKEFILE TRUE)
-  set(VERBOSE TRUE)
-endif()
 
 #
 # Installation behaviour changes
