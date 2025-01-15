84320833c474889821d82de79c70a2154ce53a91
FiniteReality <digpoealternate@gmail.com>
2025-01-15 03:37:31 +0000

    [MAYBE WORKING?] compiler magic on linux

--- Graphics/GraphicsSurfaceContext.cpp
+++ Graphics/GraphicsSurfaceContext.cpp
@@ -2,37 +2,14 @@
 
 namespace NovelRT::Graphics
 {
-    template<typename TBackend>
-    GraphicsSurfaceContext<TBackend>::GraphicsSurfaceContext(std::shared_ptr<BackendSurfaceContextType> implementation,
-                                                             std::shared_ptr<IGraphicsSurface> surface,
-                                                             std::shared_ptr<GraphicsProvider<TBackend>> provider)
-        : _implementation(implementation), _surface(surface), _provider(provider)
-    {
-        if (_surface == nullptr)
-        {
-            throw Exceptions::NullPointerException("The supplied IGraphicsSurface is nullptr.");
-        }
-
-        if (_provider == nullptr)
-        {
-            throw Exceptions::NullPointerException("The supplied GraphicsProvider is nullptr.");
-        }
-    }
+    /*template<typename TBackend>
+    GraphicsSurfaceContext<TBackend>::
 
     template<typename TBackend>
-    std::shared_ptr<IGraphicsSurface> GraphicsSurfaceContext<TBackend>::GetSurface() const noexcept
-    {
-        return _surface;
-    }
+    std::shared_ptr<IGraphicsSurface> GraphicsSurfaceContext<TBackend>::
 
     template<typename TBackend>
-    std::shared_ptr<GraphicsProvider<TBackend>> GraphicsSurfaceContext<TBackend>::GetProvider() const noexcept
-    {
-        return _provider;
-    }
+    std::shared_ptr<GraphicsProvider<TBackend>> GraphicsSurfaceContext<TBackend>::
 
-    template<typename TBackend> void* GraphicsSurfaceContext<TBackend>::GetSurfaceContextHandleUntyped()
-    {
-        return _implementation->GetSurfaceContextHandleUntyped();
-    }
+    template<typename TBackend> void* GraphicsSurfaceContext<TBackend>::*/
 }
--- Graphics/include/NovelRT/Graphics/GraphicsProvider.hpp
+++ Graphics/include/NovelRT/Graphics/GraphicsProvider.hpp
@@ -10,13 +10,73 @@
 namespace NovelRT::Graphics
 {
     template<typename TBackend> class GraphicsAdapter;
-
     template<typename TBackend> struct GraphicsBackendTraits;
+    template<typename TBackend> class GraphicsProvider;
+}
 
+namespace NovelRT::Graphics::details
+{
+    template <typename TBackend> struct GraphicsAdapterIterator
+    {
+        private:
+            using BackendProviderType = typename GraphicsBackendTraits<TBackend>::ProviderType;
+            using BackendIteratorType = decltype(std::declval<BackendProviderType>().begin());
+
+            BackendIteratorType _iterator;
+            std::shared_ptr<GraphicsProvider<TBackend>> _provider;
+
+        public:
+            GraphicsAdapterIterator(BackendIteratorType const& it, std::shared_ptr<GraphicsProvider<TBackend>> provider)
+                : _iterator(it), _provider(provider)
+            { }
+
+            using difference_type = typename std::iterator_traits<BackendIteratorType>::difference_type;
+            using value_type = std::shared_ptr<GraphicsAdapter<TBackend>>;
+            using pointer = void;
+            using reference = void;
+            using iterator_category = std::input_iterator_tag;
+
+            inline bool operator==(GraphicsAdapterIterator<TBackend> const& other)
+            {
+                return _provider == other._provider
+                    && _iterator == other._iterator;
+            }
+
+            inline bool operator!=(GraphicsAdapterIterator<TBackend> const& other)
+            {
+                return _provider != other._provider
+                    || _iterator != other._iterator;
+            }
+
+            inline auto operator*() const
+            {
+                return std::make_shared<GraphicsAdapter<TBackend>>(*_iterator, _provider);
+            }
+
+            inline auto operator++()
+            {
+                ++_iterator;
+                return *this;
+            }
+
+            inline auto operator++(int)
+            {
+                auto prev = *this;
+                ++_iterator;
+                return prev;
+            }
+    };
+}
+
+
+namespace NovelRT::Graphics
+{
     template<typename TBackend> class GraphicsProvider : public std::enable_shared_from_this<GraphicsProvider<TBackend>>
     {
     public:
         using BackendProviderType = typename GraphicsBackendTraits<TBackend>::ProviderType;
+
+        using iterator = typename details::GraphicsAdapterIterator<TBackend>;
 
     private:
         std::shared_ptr<BackendProviderType> _implementation;
@@ -30,7 +90,7 @@ namespace NovelRT::Graphics
             : _implementation(implementation), _debugModeEnabled(EngineConfig::EnableDebugOutputFromEngineInternals())
         {
         }
-        
+
         virtual ~GraphicsProvider() = default;
 
         [[nodiscard]] bool GetDebugModeEnabled() const noexcept
@@ -38,14 +98,14 @@ namespace NovelRT::Graphics
             return _debugModeEnabled;
         }
 
-        [[nodiscard]] typename std::vector<std::shared_ptr<GraphicsAdapter<TBackend>>>::iterator begin() noexcept
+        [[nodiscard]] iterator begin() noexcept
         {
-            return _implementation->begin();
+            return iterator{_implementation->begin(), this->shared_from_this()};
         }
 
-        [[nodiscard]] typename std::vector<std::shared_ptr<GraphicsAdapter<TBackend>>>::iterator end() noexcept
+        [[nodiscard]] iterator end() noexcept
         {
-            return _implementation->end();
+            return iterator{_implementation->end(), this->shared_from_this()};
         }
 
         [[nodiscard]] uint32_t GetApiVersion() const noexcept
