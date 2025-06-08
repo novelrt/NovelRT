7b2217861d2aeb1b626b345e1512d7deeb0594e5
FiniteReality <digpoealternate@gmail.com>
2025-06-08 23:00:38 +0100

    add bitflag operators header in all flag enum headers

--- Graphics/Core/include/NovelRT/Graphics/GraphicsMemoryAccessMode.hpp
+++ Graphics/Core/include/NovelRT/Graphics/GraphicsMemoryAccessMode.hpp
@@ -3,6 +3,8 @@
 // Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root
 // for more information.
 
+#include <NovelRT/Utilities/Operators.hpp>
+
 #include <cstdint>
 
 namespace NovelRT::Graphics
