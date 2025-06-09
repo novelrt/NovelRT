2762549d4e6adc432fa2ec4184ed6c021a512753
FiniteReality <digpoealternate@gmail.com>
2025-06-09 03:58:45 +0100

    Return to the original scoring logic, but pass the extensions via parameter

--- Graphics/Vulkan/VulkanGraphicsAdapterSelector.cpp
+++ Graphics/Vulkan/VulkanGraphicsAdapterSelector.cpp
@@ -3,7 +3,6 @@
 
 // TODO: clean this up good grief
 #include <NovelRT/Exceptions/NotSupportedException.hpp>
-
 #include <NovelRT/Graphics/Vulkan/QueueFamilyIndices.hpp>
 #include <NovelRT/Graphics/Vulkan/SwapChainSupportDetails.hpp>
 #include <NovelRT/Graphics/Vulkan/Utilities/Support.hpp>
@@ -13,8 +12,6 @@
 #include <NovelRT/Graphics/Vulkan/VulkanGraphicsSurfaceContext.hpp>
 
 #include <algorithm>
-#include <cmath>
-#include <functional>
 #include <string>
 #include <vector>
 
@@ -24,6 +21,42 @@ namespace NovelRT::Graphics::Vulkan
     using VulkanGraphicsDevice = GraphicsDevice<Vulkan::VulkanGraphicsBackend>;
     using VulkanGraphicsSurfaceContext = GraphicsSurfaceContext<Vulkan::VulkanGraphicsBackend>;
     using VulkanGraphicsProvider = GraphicsProvider<Vulkan::VulkanGraphicsBackend>;
+
+    static int32_t GetOptionalExtensionSupportScore(const std::vector<std::string>& availableDeviceExtensions,
+                                                    const std::vector<std::string>& optionalDeviceExtensions)
+    {
+        // If the user didn't request any optional extensions, don't give them a score.
+        if (optionalDeviceExtensions.empty())
+        {
+            return 0;
+        }
+
+        auto isOptionalExtension = [begin = optionalDeviceExtensions.begin(), end = optionalDeviceExtensions.end()](
+                                       const auto& it) { return std::find(begin, end, it) != end; };
+        auto foundOptionalExtensions =
+            std::count_if(availableDeviceExtensions.begin(), availableDeviceExtensions.end(), isOptionalExtension);
+
+        const float percentageStep = (1.0f / static_cast<float>(optionalDeviceExtensions.size())) * 100;
+        return static_cast<int32_t>(foundOptionalExtensions * percentageStep);
+    }
+
+    static bool CheckRequiredExtensionSupport(const std::vector<std::string>& availableDeviceExtensions,
+                                              const std::vector<std::string>& requiredDeviceExtensions)
+    {
+        // If the user didn't request any required extensions, pass immediately.
+        if (requiredDeviceExtensions.empty())
+        {
+            return true;
+        }
+
+        auto isRequiredExtension = [begin = requiredDeviceExtensions.begin(), end = requiredDeviceExtensions.end()](
+                                       const auto& it) { return std::find(begin, end, it) != end; };
+        auto foundRequiredExtensions =
+            std::count_if(availableDeviceExtensions.begin(), availableDeviceExtensions.end(), isRequiredExtension);
+
+        return static_cast<decltype(requiredDeviceExtensions.size())>(foundRequiredExtensions) ==
+               requiredDeviceExtensions.size();
+    }
 
     static int32_t RateDeviceSuitability(const VulkanGraphicsAdapter& adapter,
                                          const VulkanGraphicsSurfaceContext& surfaceContext,
@@ -35,20 +68,14 @@ namespace NovelRT::Graphics::Vulkan
 
         std::vector<VkExtensionProperties> availableExtensions(extensionCount);
         std::vector<std::string> extensionNames(extensionCount);
+
         vkEnumerateDeviceExtensionProperties(adapter.GetPhysicalDevice(), nullptr, &extensionCount,
                                              availableExtensions.data());
         std::transform(availableExtensions.begin(), availableExtensions.end(), extensionNames.begin(),
                        [](const auto& extension) { return std::string{extension.extensionName}; });
 
-        auto isRequiredExtension = [begin = requiredDeviceExtensions.begin(), end = requiredDeviceExtensions.end()](
-                                       const auto& it) { return std::find(begin, end, it) != end; };
-        auto firstNonRequiredExtension =
-            std::partition(extensionNames.begin(), extensionNames.end(), isRequiredExtension);
-        auto numberOfRequiredExtensions = static_cast<std::vector<std::string>::size_type>(
-            std::abs(std::distance(extensionNames.begin(), firstNonRequiredExtension)));
-
-        // If we don't have all the required extensions, the adapter is useless
-        if (numberOfRequiredExtensions < requiredDeviceExtensions.size())
+        // If we're missing required extensions, the adapter is useless.
+        if (!CheckRequiredExtensionSupport(extensionNames, requiredDeviceExtensions))
         {
             return -1;
         }
@@ -64,36 +91,23 @@ namespace NovelRT::Graphics::Vulkan
             return -1;
         }
 
-        auto isOptionalExtension = [begin = optionalDeviceExtensions.begin(), end = optionalDeviceExtensions.end()](
-                                       const auto& it) { return std::find(begin, end, it) != end; };
-        auto firstNonNecessaryExtension =
-            std::partition(firstNonRequiredExtension, extensionNames.end(), isOptionalExtension);
-        auto numberOfOptionalExtensions = static_cast<std::vector<std::string>::size_type>(
-            std::abs(std::distance(firstNonRequiredExtension, firstNonNecessaryExtension)));
-
-        // Our score starts at how many optional extensions we have,
-        int32_t score = numberOfOptionalExtensions;
-
         VkPhysicalDeviceProperties deviceProperties;
         VkPhysicalDeviceFeatures deviceFeatures;
         vkGetPhysicalDeviceProperties(adapter.GetPhysicalDevice(), &deviceProperties);
         vkGetPhysicalDeviceFeatures(adapter.GetPhysicalDevice(), &deviceFeatures);
 
-        // The bit position of the largest power-of-two sized texture that can be created on this device
-        // That is, 1 << maxPo2TextureSizeBitOffset will return the actual texture size.
-        int32_t maxPo2TextureSizeBitOffset =
-            std::log2(static_cast<int32_t>(deviceProperties.limits.maxImageDimension2D));
+        int32_t score = 0;
 
-        // If the device can create larger textures, it's usually more powerful.
-        score += maxPo2TextureSizeBitOffset * std::max(optionalDeviceExtensions.size(), 1ul);
-
-        // However, we want to prefer dedicated hardware even if software emulation an create something even larger.
+        // Prefer discrete GPUs where possible
         if (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
         {
-            // N.B. There's a bunch of conflicting information, but it's rather unlikely that a device can create a
-            // texture that is 2^32 pixels large.
-            score += 32 * std::max(optionalDeviceExtensions.size(), 1ul);
+            score += 1000;
         }
+
+        // Prefer devices with larger texture support
+        score += static_cast<int32_t>(deviceProperties.limits.maxImageDimension2D);
+        // Prefer devices which support more optional extensions
+        score += GetOptionalExtensionSupportScore(extensionNames, optionalDeviceExtensions);
 
         return score;
     }
