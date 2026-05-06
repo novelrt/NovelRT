72719908a5d8d5b9a1229d15124252e798bd34aa
Matt Jones <matthewj866@gmail.com>
2026-05-06 01:07:26 +0100

    Attempt n+1 at making this compile under CI.

--- Ecs/Core/include/NovelRT/Ecs/ImplDetail.hpp
+++ Ecs/Core/include/NovelRT/Ecs/ImplDetail.hpp
@@ -3,6 +3,19 @@
 // Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root
 // for more information.
 
+namespace NovelRT
+{
+    namespace Timing
+    {
+        class Timestamp;
+    }
+
+    namespace Ecs
+    {
+        class Catalogue;
+    }
+}
+
 namespace NovelRT::Ecs::Detail
 {
     template<typename TWork, typename TCompletion>
