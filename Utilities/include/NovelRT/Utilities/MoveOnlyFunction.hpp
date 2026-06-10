35d9c9fcbe46d279539129811fb788ba6d504b74
WaggingWalrus <WaggingWalrus@gmail.com>
2026-06-10 13:43:29 +0200

    Include functional in moveOnlyFunction to fix breaking ubuntu pipeline

--- Utilities/include/NovelRT/Utilities/MoveOnlyFunction.hpp
+++ Utilities/include/NovelRT/Utilities/MoveOnlyFunction.hpp
@@ -3,6 +3,8 @@
 // Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root
 // for more information.
 
+#include <functional>
+
 namespace NovelRT::Utilities
 {
     template<typename...>
